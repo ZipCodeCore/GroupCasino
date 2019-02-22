7ec40d807e076f261e953a2f4036a851d6e2e03e
Eric Jefferson <ejefferson50@gmail.com>
2019-02-22 18:15:06 -0500

    Guest Account Done

--- src/main/java/io/zipcoder/casino/Models/GuestAccount.java
+++ src/main/java/io/zipcoder/casino/Models/GuestAccount.java
@@ -7,22 +7,51 @@ public class GuestAccount {
     private String name;
     //private String password;
     private Double accountBalance;
-    private Long idNumber;
+    private Integer id;
+
+
+    public GuestAccount(String name, Integer id, Double accountBalance) {
+        this.name = name;
+        this.id = id;
+        this.accountBalance = accountBalance;
+
+    }
 
     public String getName() {
         return name;
-    }
-
-    public Double getAccountBalance() {
-        return accountBalance;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setAccountBalance(Double accountBalance) {
+    public Integer getId() {
+        return id;
+    }
+
+
+    public Double getAccountBalance() {
+        return this.accountBalance;
+    }
+
+    /*public void setAccountBalance(Double balance) {
         this.accountBalance = accountBalance;
-        Casino.console.getDoubleInput("Hello");
+    }*/
+
+    public String toStringGuestAccount(Integer id) {
+
+        return String.format("%s, %d, $%.2f\n ", name, id, accountBalance);
+
+    }
+
+    public Double addFunds(Double funds){
+
+        return this.accountBalance + funds;
+    }
+
+    public Double removeFunds(Double funds){
+
+        return this.accountBalance - funds;
     }
 }
+
