f53a8c4d49b32f8ad2f3c9f3a28c2de2c4f37662
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-22 18:30:09 -0500

    to String() added for Card

--- src/main/java/io/zipcoder/casino/Cards/Card.java
+++ src/main/java/io/zipcoder/casino/Cards/Card.java
@@ -4,17 +4,24 @@ public class Card {
 
     private Suit suit;
     private Rank rank;
+
     public Card(Suit suit, Rank rank) {
         this.suit = suit;
         this.rank = rank;
     }
 
     public Suit getSuit() {
+
         return suit;
     }
 
-
     public Rank getRank() {
+
         return rank;
+    }
+
+    public String toString() {
+        return ""+rank+" of "+suit;
+
     }
 }
