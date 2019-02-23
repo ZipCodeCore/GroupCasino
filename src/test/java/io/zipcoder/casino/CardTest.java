45cd0e95da07effe3fc2153ccf49d6858d5564e3
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-02-22 19:11:25 -0500

    card tests

---
+++ src/test/java/io/zipcoder/casino/CardTest.java
@@ -0,0 +1,48 @@
+package io.zipcoder.casino;
+
+import io.zipcoder.casino.Cards.Card;
+import io.zipcoder.casino.Cards.Rank;
+import io.zipcoder.casino.Cards.Suit;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CardTest {
+
+    @Test
+
+    public void testCardConstructor() {
+
+        //Given
+        Rank rank = Rank.ACE;
+        Suit suit = Suit.DIAMONDS;
+        Card testCard = new Card(suit,rank);
+
+        //When
+        Rank actualRank = testCard.getRank();
+        Suit actualSuit = testCard.getSuit();
+
+        //Then
+        Assert.assertEquals(rank,actualRank);
+        Assert.assertEquals(suit,actualSuit);
+
+    }
+
+    @Test
+    public void testCardSetMethod() {
+
+        //Given
+        Rank rank = Rank.FIVE;
+        Suit suit = Suit.SPADES;
+        Card testCard = new Card(suit,rank);
+
+        //When
+        Rank actualRank = testCard.getRank();
+        Suit actualSuit = testCard.getSuit();
+        String expected = "" + rank + " of "+suit;
+        //Then
+        Assert.assertEquals(expected,testCard.toString());
+        Assert.assertEquals(suit,actualSuit);
+
+    }
+
+}
