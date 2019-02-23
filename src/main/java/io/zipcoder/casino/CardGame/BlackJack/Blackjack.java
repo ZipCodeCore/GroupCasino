d8d3879364d5423d39631a1f4fc5d3bbdad5adbf
Delenda Joseph <delendajoseph@zipcodersMBP55.localdomain>
2019-02-23 12:25:19 -0500

    created drawCard method and deck

--- src/main/java/io/zipcoder/casino/CardGame/BlackJack/Blackjack.java
+++ src/main/java/io/zipcoder/casino/CardGame/BlackJack/Blackjack.java
@@ -1,17 +1,20 @@
 package io.zipcoder.casino.CardGame.BlackJack;
 
 import io.zipcoder.casino.CardGame.CardGame;
+import io.zipcoder.casino.CardGame.Cards.Deck;
 import io.zipcoder.casino.GamblingGame;
 
 public class Blackjack extends CardGame implements GamblingGame {
     private double pot;
     private BlackjackPlayer blackjackPlayer;
+    private Deck deck = new Deck();
 
     public void takeBet(double amount) {
         pot += amount;
     }
 
     public void deal() {
+        deck.drawCard();
     }
 
     public double payout() {
--- src/main/java/io/zipcoder/casino/CardGame/Cards/Deck.java
+++ src/main/java/io/zipcoder/casino/CardGame/Cards/Deck.java
@@ -5,9 +5,33 @@ public class Deck {
     private Deque<Card> cards;
     private int size;
 
+    public Deck() {
+        createDeck();
+
+    }
+
+    public Card drawCard() {
+        return cards.pop();
+
+    }
+    public Deque<Card> getDeck(){
+        return cards;
+
+    }
     public void shuffle() {
     }
 
     public void deal() {
     }
+
+    private void createDeck() {
+        for (Suit s : Suit.values()) {
+            for (Face f : Face.values()) {
+                cards.add(new Card(f,s));
+
+            }
+        }
+    }
 }
+
+
