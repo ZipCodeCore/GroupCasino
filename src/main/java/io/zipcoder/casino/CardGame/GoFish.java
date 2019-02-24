03ae5884c6efb30aceff3f2d6e4d74b9e8713337
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-24 13:49:55 -0500

    Go Fish and Player changes

--- src/main/java/io/zipcoder/casino/CardGame/GoFish.java
+++ src/main/java/io/zipcoder/casino/CardGame/GoFish.java
@@ -15,22 +15,52 @@ public class GoFish extends CardGame {
     public Hand dealersHand;
 
 
-    public GoFish(Player player){
+    public GoFish(Player player) {
 
-        this.goFishPlayer =  new GoFishPlayer(player);
+        this.goFishPlayer = new GoFishPlayer(player);
 
 
         //this.dealer = new GoFishPlayer( new Player("dealer", 0.0)); // Cara is adding the Player Constructor.
 
 
     }
+    //While ((player.hand is empty || Dealer.hand is empty) && deck is Empty)
+    // The two go Fish players are passed to the play method.
+    //Deal cards- Deck.deal (7) -> returns array list of cards.
 
-   // The two go Fish players are passed to the
+    //Player's turn
+    //Display hand  call show method.
+    //Call IO console  to take input from the player which card they want to request.
+    //Request for card
+    // Boolean check Hand (player/Dealer, enum face)
+
+    //If (checkHand) {
+    // ArrayList <Cards> cardsToReturn = new ArrayList();
+    //for (Card c: dealer/player.hand) {
+    // if (c.getFace == Enum face){
+    // cardsToReturn.add(c);
+    //dealer/player hand.remove(c) }
+
+    //Dealers's turn
+    // get dealers' hand size
+    // random index between 
+
+    //Else  -> Go Fish -> player.draw from the deck
+    //If the face of draw equals the face requested then // Request for Card  and continue
+
+
     public void play() {
 
 
+    }
 
+    // The show method shows the hand of a particular player/dealer
+//    System.out.println('\u2665');// hearts
+//    System.out.println('\u2666');//Diamond
+//    System.out.println('\u2660'); //Spade
+//    System.out.println('\u2663'); // club
 
+    public void showHand(GoFishPlayer player) {
 
 
     }
