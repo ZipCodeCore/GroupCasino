70af351c5aa8ab7f594b9922a26f6f60777ecbf3
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-02-24 18:17:48 -0500

    Initial craps changes with methods for betting

--- src/main/java/io/zipcoder/casino/DiceGame/Craps.java
+++ src/main/java/io/zipcoder/casino/DiceGame/Craps.java
@@ -1,13 +1,25 @@
 package io.zipcoder.casino.DiceGame;
 
 import io.zipcoder.casino.GamblingGame;
+import io.zipcoder.casino.utilities.Console;
 
 public class Craps extends DiceGame implements GamblingGame {
-    private double pot;
+
     private int buttonNumber;
     private CrapsPlayer crapsPlayer;
+    private double passLinePot;
+    private double passLineOddsPot;
+    private double comeLinePot;
+    private double comeLineOddsPot;
+    private double hardWaysPot;
+    private double fieledPot;
+    private double placePot;
 
     public void play() {
+        boolean play = false;
+        Console crapsConsole = Console.getInstance();
+
+
     }
 
     public void walkAway() {
@@ -18,7 +30,40 @@ public class Craps extends DiceGame implements GamblingGame {
         return 0;
     }
 
-    public void takeBet(double amount) {
+
+    public void passLineBet(Double amount){
+        crapsPlayer.bet(amount);
+        passLinePot += amount;
+    }
+
+    public void passLineOddsBet(Double amount){
+        crapsPlayer.bet(amount);
+        passLineOddsPot += amount;
+    }
+
+    public void comeLineBet(Double amount){
+        crapsPlayer.bet(amount);
+        comeLinePot += amount;
+    }
+
+    public void comeLineOddsBet(Double amount){
+        crapsPlayer.bet(amount);
+        comeLineOddsPot += amount;
+    }
+
+    public void hardWayBet(Double amount){
+        crapsPlayer.bet(amount);
+        hardWaysPot += amount;
+    }
+
+    public void fieldBet(Double amount){
+        crapsPlayer.bet(amount);
+        fieledPot += amount;
+    }
+
+    public void placeLineBet(Double amount){
+        crapsPlayer.bet(amount);
+        placePot += amount;
     }
 
 
