4b39473e117ab6c62330ac6650b5ede7a76f5a96
Delenda Joseph <delendajoseph@zipcodersMBP55.localdomain>
2019-02-26 11:27:02 -0500

    added stub for blacksjack unit test

--- src/test/java/io/zipcoder/casino/BlackjackTest.java
+++ src/test/java/io/zipcoder/casino/BlackjackTest.java
@@ -1,10 +1,26 @@
 package io.zipcoder.casino;
 import io.zipcoder.casino.CardGame.BlackJack.Blackjack;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BlackjackTest {
-   
+
+    private Player player;
+
+    @Before
+    public void setUp(){
+        player = new Player("name", 1000);
+    }
+    @Test
+    public void constructorTest(){
+        //Given
+        Blackjack blackjack = new Blackjack(player);
+
+        //When
+
+        //Then
+    }
 
     @Test
     public void payout() {
