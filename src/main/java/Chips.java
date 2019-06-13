95db68c3a09f8dc4c0e0accdced2e2e4e42a84f5
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-13 15:04:57 -0400

    added chips and house

--- src/main/java/Chips.java
+++ src/main/java/Chips.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Chips {
+    private Integer chips;
+    public Chips(Double moneyForChips) {
+        this.chips = (int)Math.floor(moneyForChips/5.0);
+    }
 }
--- src/main/java/House.java
+++ src/main/java/House.java
@@ -2,6 +2,6 @@ public class House implements Dealer {
 
     public void payout() {}
     public void takeBets() {}
-    public Integer moneyToChips(Double moneyAmount, Player player) { return 0; }
+    public Chips moneyToChips(Double moneyAmount, Player player) { return 0; }
 
 }
