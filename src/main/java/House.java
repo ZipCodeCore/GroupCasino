e64917d167422e0eeedc482ef7a06c14d941da5a
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-13 14:51:35 -0400

    ascii card, house edits

--- src/main/java/House.java
+++ src/main/java/House.java
@@ -2,5 +2,6 @@ public class House implements Dealer {
 
     public void payout() {}
     public void takeBets() {}
+    public Integer moneyToChips(Double moneyAmount, Player player) { return 0; }
 
 }
