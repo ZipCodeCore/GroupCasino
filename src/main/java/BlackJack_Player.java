86c1b583e0687fa40dab867c9cb4bf5e4260f15c
Stefun <hawkd215@gmail.com>
2019-06-14 18:25:58 -0400

    error free

--- src/main/java/BlackJack_Player.java
+++ src/main/java/BlackJack_Player.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
-public class BlackJack_Player extends Player implements Gambling, Dealer {
+public class BlackJack_Player extends Player implements Gambling {
 
     private Person player;
     Integer bet;
-    Hand playerHand = dealHand(1);
+    
 
 
 
@@ -15,12 +15,12 @@ public class BlackJack_Player extends Player implements Gambling, Dealer {
     public Boolean didWin(){ return false;}
     public void makeMove(){}
     public void leaveGame(){}
-    public Boolean canSplit(){
-        ArrayList<Card> playerHand =  new ArrayList<>(5);
+  /*  public Boolean canSplit(){
+
         if(playerHand.get(0) == playerHand.get(1)){
             return true;
         }
-        return false;}
+        return false;}*/
     public void split() {}
     public void hitOrStay() {}
     public void doubleDown() {}
--- src/test/java/BlackJack_PlayerTest.java
+++ src/test/java/BlackJack_PlayerTest.java
@@ -11,7 +11,7 @@ public class BlackJack_PlayerTest {
 
     @Before
     public void setUp() throws Exception {
-        BlackJack_Player player1 = new BlackJack_Player(new Person(1000,"Stefun", 34));
+        BlackJack_Player player1 = new BlackJack_Player(new Person(1000.00,"Stefun", 34));
         BlackJack_Player [] players = new BlackJack_Player[3];
         players[0] = player1;
         BlackJack blackJack1 = new BlackJack(players, 5);
@@ -21,9 +21,6 @@ public class BlackJack_PlayerTest {
     @After
     public void tearDown() throws Exception {
     }
-
-    @Test
-
 
 
     @Test
