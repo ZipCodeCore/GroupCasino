ebe8467e9c00e38e8744ff9096d00d1e7dddb137
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-14 12:06:53 -0400

    Refactored card

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,6 +1,4 @@
 public class Card {
-    enum Rank {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
-    enum Suit {HEART, DIAMOND, SPADE, CLUB}
 
     private Boolean faceDown;
     private Rank rank;
@@ -32,7 +30,7 @@ public class Card {
         return suit;
     }
 
-    public String getValue() {return "";}
+    public Integer getValue() {return 0;}
 
 
     public void flip() {}
