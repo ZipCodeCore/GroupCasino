c2b93babb2c9f357b9ede6bd9f75987e5810496e
deltagphys <cty.jgates@gmail.com>
2019-11-01 20:29:49 -0400

    merge card compare and toString and test

--- src/main/java/io/zipcoder/casino/Card.java
+++ src/main/java/io/zipcoder/casino/Card.java
@@ -1,11 +1,36 @@
 package io.zipcoder.casino;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Card implements Comparable<Card>{
 
     private String rank;
-    private String suit;
+    private String suit; //H,C,S,D
+    private static final ArrayList<String> ranks;
+    private static final HashMap<String,String> suitSymbols;
+    static {
+        ranks = new ArrayList<String>(Arrays.asList(new String[]{"2","3","4","5","6","7","8","9","10","J","Q","K","A"}));
+        suitSymbols = new HashMap<String, String>();
+        suitSymbols.put("H","\\u2665");
+        suitSymbols.put("S","\\u2660");
+        suitSymbols.put("C","\\u2663");
+        suitSymbols.put("D","\\u2666");
+    }
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
 
     public int compareTo (Card card2) {
-        return 0;
+        return Integer.compare(Card.ranks.indexOf(this.rank), Card.ranks.indexOf(card2.rank));
+    }
+
+    @Override
+    public String toString() {
+        return this.rank + suitSymbols.get(this.suit);
     }
 }
---
+++ src/test/java/io/zipcoder/casino/CardTest.java
@@ -0,0 +1,74 @@
+package io.zipcoder.casino;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+
+public class CardTest {
+
+    private Card card1;
+
+    @Before
+    public void setUp() throws Exception {
+        card1 = new Card("7","H");
+    }
+
+    @Test
+    public void compareToTest() {
+        Assert.assertTrue(card1.compareTo(new Card("8","H")) == -1);
+    }
+
+    @Test
+    public void compareToTest2() {
+        Assert.assertTrue(card1.compareTo(new Card("7","S")) == 0);
+    }
+
+    @Test
+    public void compareToTest3() {
+        Assert.assertTrue(card1.compareTo(new Card("3","C")) == 1);
+    }
+
+    @Test
+    public void compareToTest4() {
+        Assert.assertTrue(card1.compareTo(new Card("A","D")) == -1);
+    }
+
+    @Test
+    public void testToString() {
+        Assert.assertEquals("A\\u2666", new Card("A","D").toString());
+    }
+
+    @Test
+    public void testToString2() {
+        Assert.assertEquals("7\\u2665", card1.toString());
+    }
+
+    @Test
+    public void testToString3() {
+        Assert.assertEquals("3\\u2663", new Card("3","C").toString());
+    }
+
+    @Test
+    public void testToString4() {
+        Assert.assertEquals("7\\u2660", new Card("7","S").toString());
+    }
+
+    @Test
+    public void sortTest() {
+        Card[] cards = new Card[] {card1, new Card("3","C"), new Card("7","S"), new Card("A","D"), new Card("K","S"), new Card("J","C"), new Card("Q","D")};
+        Card[] expected = new Card[] {new Card("3","C"), card1,  new Card("7","S"), new Card("J","C"), new Card("Q","D"), new Card("K","S"), new Card("A","D")};
+        ArrayList<Card> cardAL = new ArrayList<Card>(Arrays.asList(cards));
+        Collections.sort(cardAL);
+
+        for (int i = 0; i < cards.length; i++) {
+            Assert.assertTrue(expected[i].compareTo(cardAL.get(i)) == 0);
+        }
+
+    }
+}
