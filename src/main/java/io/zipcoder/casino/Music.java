0d62e24e64334e3313d90ef3bff5a34f1970d79f
SandyPham823 <sandydupham@gmail.com>
2019-11-02 16:25:32 -0400

    Added all sound files needed and now creating a stop method to stop audio output

--- src/main/java/io/zipcoder/casino/Music.java
+++ src/main/java/io/zipcoder/casino/Music.java
@@ -6,24 +6,41 @@ import sun.audio.AudioStream;
 import javax.swing.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 public class Music {
 
-    public static void playMusic(String filepath)
-    {
+    public static void main(String[] args){
+        playMusic("src/music/(Roulette) Kirby Star Allies Music.wav");
+        stopMusic("src/music/(Roulette) Kirby Star Allies Music.wav");
+    }
+
+    public static void playMusic(String filepath) {
         InputStream music;
-        try
-        {
+        try {
             music = new FileInputStream(new File(filepath));
             AudioStream audios = new AudioStream(music);
             AudioPlayer.player.start(audios);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error");
         }
-        catch (Exception e)
+    }
+    public static void stopMusic(String filepath) {
+        InputStream music;
+        try {
+            music = new FileInputStream(new File(filepath));
+            AudioStream audios = new AudioStream(music);
+            AudioPlayer.player.start(audios);
+            if (audios != null)
+            {
+                AudioPlayer.player.stop(audios);
+            }
+        }
+        catch (IOException e)
         {
-            JOptionPane.showMessageDialog(null,"Error");
+            System.err.println(e);
         }
-
     }
 }
 
