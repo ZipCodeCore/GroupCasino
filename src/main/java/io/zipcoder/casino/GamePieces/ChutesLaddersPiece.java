d23bde73a05bb57d828d247f133709d0142fd689
Ryan <ryan@rmiller.dev>
2019-11-03 17:13:54 -0500

    Spinner and Chutes/ladders piece

---
+++ src/main/java/io/zipcoder/casino/GamePieces/ChutesLaddersPiece.java
@@ -0,0 +1,13 @@
+package io.zipcoder.casino.GamePieces;
+
+public class ChutesLaddersPiece {
+    private Integer currentPosition = 0;
+
+    public Integer getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Integer currentPosition) {
+        this.currentPosition = currentPosition;
+    }
+}
