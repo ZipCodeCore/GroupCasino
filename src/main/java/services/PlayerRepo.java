e347032bd02238d3451ce44fcc880a126b3821a6
ushanil <ush.gopi@gmail.com>
2019-11-04 16:27:52 -0500

    PlayerRepo.java

--- src/main/java/services/PlayerRepo.java
+++ src/main/java/services/PlayerRepo.java
@@ -12,16 +12,30 @@ public class PlayerRepo {
 
     }
 
-    public Player getPlayer() {
-        return null;
+    public ArrayList<Player> getPlayers() //changedtogetPlayers and returns arrayList
+    {
+        return playerRepo;
     }
 
-    public Player getPlayerByName(String name) {
-        return null;
+    public Player getPlayerByName(String name)
+    {
+
+        Player p1 = new Player();
+        for (Player player1: playerRepo)
+        {
+            if (playerRepo.contains(name))
+            {
+                p1 = player1;
+                break;
+            }
+        }
+        return p1;
+
     }
 
-    public Boolean addPlayer() {
-        return null;
+    public Boolean addPlayer(Player p1)  //added argument Player p1 to this method
+    {
+        if (playerRepo.add(p1)) return true; else return false;
     }
 
 
