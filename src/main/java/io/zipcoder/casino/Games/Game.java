db3468fe68581aac5a00644e88c7ee89914e810a
Ryan <ryan@rmiller.dev>
2019-11-04 16:11:36 -0500

    stub game interfaces

--- src/main/java/io/zipcoder/casino/Games/Game.java
+++ src/main/java/io/zipcoder/casino/Games/Game.java
@@ -1,4 +1,11 @@
 package io.zipcoder.casino.Games;
 
+import io.zipcoder.casino.Player;
+
 public interface Game {
+
+    void approachTable(Player currentPLayer);
+    void runGame(Player currentPlayer);
+    void exitGame();
+
 }
