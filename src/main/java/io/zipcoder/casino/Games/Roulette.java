5cc24b2e430f997ad27f483d8fe6a54557be093f
Ryan <ryan@rmiller.dev>
2019-11-04 16:25:55 -0500

    implement interface methods

--- src/main/java/io/zipcoder/casino/Games/Roulette.java
+++ src/main/java/io/zipcoder/casino/Games/Roulette.java
@@ -1,4 +1,20 @@
 package io.zipcoder.casino.Games;
 
+import io.zipcoder.casino.Player;
+
 public class Roulette implements Game {
+    @Override
+    public void approachTable(Player currentPLayer) {
+
+    }
+
+    @Override
+    public void runGame(Player currentPlayer) {
+
+    }
+
+    @Override
+    public void exitGame() {
+
+    }
 }
