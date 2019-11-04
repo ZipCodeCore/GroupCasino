508af10ad0d6a950b3fef04a9af41d6096726505
Amendoza20 <20.angel.mendoza@gmail.com>
2019-11-04 16:52:24 -0500

    push

--- src/main/java/io/zipcoder/casino/Games/Slots.java
+++ src/main/java/io/zipcoder/casino/Games/Slots.java
@@ -2,17 +2,13 @@ package io.zipcoder.casino.Games;
 
 import io.zipcoder.casino.Player;
 
-<<<<<<< HEAD
 public class Slots implements Game, GamblingGame {
-=======
-public class Slots implements Game {
+
     @Override
->>>>>>> 14eed1c54f5a20b9fa87dafd61c10485005169f8
     public void approachTable(Player currentPLayer) {
 
     }
 
-<<<<<<< HEAD
     public void runGame(Player currentPlayer){
         //prompting player to place bet
        //pull lever to generate random multidimensional array(s)
@@ -28,15 +24,5 @@ public class Slots implements Game {
 
     }
     public void returnWinnings(Player currentPlayer){
-=======
-    @Override
-    public void runGame(Player currentPlayer) {
-
-    }
-
-    @Override
-    public void exitGame() {
->>>>>>> 14eed1c54f5a20b9fa87dafd61c10485005169f8
-
     }
 }
