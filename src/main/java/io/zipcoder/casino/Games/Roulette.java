ad531a836a3594918faa4f063374a736b2c7137d
Mike King <michkin32@gmail.com>
2019-11-04 18:12:26 -0500

    progress on blackjack

--- src/main/java/io/zipcoder/casino/Games/Roulette.java
+++ src/main/java/io/zipcoder/casino/Games/Roulette.java
@@ -2,45 +2,33 @@ package io.zipcoder.casino.Games;
 
 import io.zipcoder.casino.Player;
 
-<<<<<<< HEAD
-public class Roulette implements Game, GamblingGame{
 
-=======
-public class Roulette implements Game {
+public class Roulette implements Game, GamblingGame{
     @Override
->>>>>>> 14eed1c54f5a20b9fa87dafd61c10485005169f8
     public void approachTable(Player currentPLayer) {
 
-    }
 
-<<<<<<< HEAD
-    public void runGame(Player currentPlayer){
         //prompting player to place bet
         // prompting to pick number
         //call spinner to generate winning number
         // if player number == winning number
         // return winnings
     }
-
+    @Override
     public void exitGame(){
 
     }
+    @Override
     public void placeBet(Player currentPlayer){
 
     }
+    @Override
     public void returnWinnings(Player currentPlayer){
 
     }
-    //
-=======
+
     @Override
     public void runGame(Player currentPlayer) {
 
     }
-
-    @Override
-    public void exitGame() {
-
-    }
->>>>>>> 14eed1c54f5a20b9fa87dafd61c10485005169f8
 }
