d81f204cc8a54de60ba3103ac7f2c7bb8d11eacf
Amendoza20 <20.angel.mendoza@gmail.com>
2019-11-04 16:35:33 -0500

    added psuedocode for slots and Roulette

--- src/main/java/io/zipcoder/casino/Games/Roulette.java
+++ src/main/java/io/zipcoder/casino/Games/Roulette.java
@@ -1,4 +1,29 @@
 package io.zipcoder.casino.Games;
 
-public class Roulette implements Game {
+import io.zipcoder.casino.Player;
+
+public class Roulette implements Game, GamblingGame{
+
+    public void approachTable(Player currentPLayer) {
+
+    }
+
+    public void runGame(Player currentPlayer){
+        //prompting player to place bet
+        // prompting to pick number
+        //call spinner to generate winning number
+        // if player number == winning number
+        // return winnings
+    }
+
+    public void exitGame(){
+
+    }
+    public void placeBet(Player currentPlayer){
+
+    }
+    public void returnWinnings(Player currentPlayer){
+
+    }
+    //
 }
--- src/main/java/io/zipcoder/casino/Games/Slots.java
+++ src/main/java/io/zipcoder/casino/Games/Slots.java
@@ -1,4 +1,27 @@
 package io.zipcoder.casino.Games;
 
-public class Slots implements Game {
+import io.zipcoder.casino.Player;
+
+public class Slots implements Game, GamblingGame {
+    public void approachTable(Player currentPLayer) {
+
+    }
+
+    public void runGame(Player currentPlayer){
+        //prompting player to place bet
+       //pull lever to generate random multidimensional array(s)
+        //checking for matches along horizontals && diagonals && verticals
+        // return winnings
+        //perf
+    }
+
+    public void exitGame(){
+
+    }
+    public void placeBet(Player currentPlayer){
+
+    }
+    public void returnWinnings(Player currentPlayer){
+
+    }
 }
