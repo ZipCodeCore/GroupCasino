8392521d68eb00e10bf95f3ac1ed3102524c46a0
MannyMonocle <eetumadu@gmail.com>
2019-11-05 11:22:20 -0500

    PlayerWarehouse as ArrayList

--- src/main/java/io/zipcoder/casino/Player/Player.java
+++ src/main/java/io/zipcoder/casino/Player/Player.java
@@ -1,4 +1,21 @@
 package io.zipcoder.casino.Player;
 
+import io.zipcoder.casino.CardGames.CardHand;
+
 public class Player {
+
+    int ID;
+    int pwd;
+    double balance;
+    CardHand cardHand;
+
+
+
+    public Player(int ID, int password){
+        this.ID = ID;
+        this.pwd = password;
+        this.balance = 0.0;
+        this.cardHand = new CardHand;
+    }
+
 }
--- src/main/java/io/zipcoder/casino/Player/PlayerWarehouse.java
+++ src/main/java/io/zipcoder/casino/Player/PlayerWarehouse.java
@@ -1,4 +1,15 @@
 package io.zipcoder.casino.Player;
 
+import java.util.ArrayList;
+
 public class PlayerWarehouse {
+
+    private ArrayList<Player> warehouse = new ArrayList<>();
+
+    public void addPlayer(int ID, int pwd){
+
+        Player newUser = new Player(ID, pwd);
+        warehouse.add(newUser);
+
+    }
 }
