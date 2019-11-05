7891f0cef6e1e7c4654df40beeabfe359eddb27d
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-05 18:07:06 -0500

    craps update

--- src/main/java/io/zipcoder/casino/DiceGames/Craps.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Craps.java
@@ -1,34 +1,37 @@
 package io.zipcoder.casino.DiceGames;
 
 import io.zipcoder.casino.Interfaces.GamblingGame;
-import io.zipcoder.casino.Interfaces.GamblingPlayer;
 import io.zipcoder.casino.Interfaces.Game;
+import io.zipcoder.casino.Player.GamblingPlayer;
+import io.zipcoder.casino.Player.Player;
 import io.zipcoder.casino.utilities.Console;
 
-public class Craps implements Game, GamblingGame,GamblingPlayer {
+public class Craps implements Game, GamblingGame {
 
-    public Craps crapsGame = new
+    private Dice[] crapsDice;
 
-    private static Console console = new Console(System.in, System.out);
+    private GamblingPlayer crapsPlayer;
 
-    private String[] availableOptionsCrapsNames = {"Roll the Die ","YO Levine the Dancing Queen", "Place Your Bets",
-                                                    "Winner Winner Chicken Dinner", "Nina Nina from Pasedena"};
+    public Craps(Player player) {
+        this.crapsPlayer = new GamblingPlayer(player);
 
-    public void user(){}
-    public void diceValues (){}
-    public void  currentRolledNumber(){}
-    public void PointNumber(){}
-    public void startGame(){}
-    public int [] roll (int[] diceValues){
-        return null;
     }
-    public void distributeWinnings(){}
-    public void evaluateDiceValue (){}
-    public void  promptLeaveGame(){}
-    public void  displayResults(){}
+
+    private boolean continuePlay;
+
+    public void startGame() {
+    }
+
+    public void promptLeaveGame() {
+
+    }
 
 
-    public void placeWage() {
+    public void displayResults() {
+    }
+
+
+    public void distributeWinnings() {
 
     }
 }
