20a3a5151aed9f617d9a2c456dcdadb82c5be3af
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-04 21:16:36 -0500

    Added info to Dice Class

--- src/main/java/io/zipcoder/casino/DiceGames/Dice.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Dice.java
@@ -1,4 +1,21 @@
 package io.zipcoder.casino.DiceGames;
 
-public class Dice {
+import io.zipcoder.casino.Interfaces.GamblingGame;
+
+import java.util.ArrayList;
+
+public abstract class Dice implements GamblingGame {
+
+    public static ArrayList<Integer> dealerDice = new ArrayList<Integer>(5);
+    public static ArrayList<Integer> playerDice = new ArrayList<Integer>(5);
+    public static ArrayList<Integer> heldDice = new ArrayList<Integer>(5);
+    public static Integer numOfDice = 0;
+
+
+    public Dice(){}
+
+    public int diceValue(){return 0;}
+
+    public static void createDiceAndroll(int[]numberOfDice){ }
 }
+
