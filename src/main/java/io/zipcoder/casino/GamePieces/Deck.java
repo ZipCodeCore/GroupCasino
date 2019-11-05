e5969482387bb6b464ecd1ac474b3454eece9eb1
Mike King <michkin32@gmail.com>
2019-11-05 11:28:10 -0500

    Progress on blackjack

--- src/main/java/io/zipcoder/casino/GamePieces/Deck.java
+++ src/main/java/io/zipcoder/casino/GamePieces/Deck.java
@@ -11,7 +11,7 @@ public class Deck {
 
     public Deck(){
         this.deck = deck;
-        for(int i =0; i < 13; i++){
+        for(int i =1; i <= 11; i++){
             CardValue value = CardValue.values()[i];
             for(int j= 0; j < 4;j++){
                 Card card= new Card(value, Suit.values()[j]);
@@ -29,7 +29,7 @@ public class Deck {
     public Card draw(){
         Card drawnCard = deck.remove(0);
 
-        console.println(drawnCard.toString());
+
         return drawnCard;
     }
 }
--- src/main/java/io/zipcoder/casino/Games/BlackJack.java
+++ src/main/java/io/zipcoder/casino/Games/BlackJack.java
@@ -6,8 +6,10 @@ import io.zipcoder.casino.GamePieces.Deck;
 import io.zipcoder.casino.utilities.Console;
 
 public class BlackJack implements Game, GamblingGame{
+
+    Deck deck = new Deck();
     Console console = new Console(System.in, System.out);
-    Card[] hand = new Card[6];
+    Card[] playerHand = new Card[6];
     Card[] dealerHand = new Card[6];
     Player dealer = new Player( "Dealer", 100000);
 
@@ -18,11 +20,15 @@ public class BlackJack implements Game, GamblingGame{
     @Override
     public void runGame(Player currentPlayer) {
         while(running)
-        console.println("Welcome to BlackJack!");
-        //draw two cards for current player and Dealer
+        console.println("Welcome to BlackJack! Let's begin!");
+        deck.shuffle();
+        initialHand();
+        viewDealerHand();
+        viewCurrentHand();
         console.println("How much would you like to los- I mean bet?");
         placeBet(currentPlayer);
         console.println("Would you like to 'hit' or 'stay'?");
+        viewDealerHand();
         viewCurrentHand();
         hitOrStay();
 
@@ -48,7 +54,10 @@ public class BlackJack implements Game, GamblingGame{
 
     }
     public void viewCurrentHand(){
-        console.println(String.valueOf(hand[0]) + " " + String.valueOf(hand[1]));
+        console.println("Your hand is " + String.valueOf(playerHand[0]) + " " + String.valueOf(playerHand[1]));
+    }
+    public void viewDealerHand(){
+        console.println("Dealer hand is " + String.valueOf(dealerHand[0]));
     }
     public void hitOrStay(){
         String playerInput = console.getStringInput(":");
@@ -61,6 +70,15 @@ public class BlackJack implements Game, GamblingGame{
         }
     }
     public void hit(){
+        if(playerHand[2] == null){
+            playerHand[2] = deck.draw();
+        }else if(playerHand[2] != null){
+            playerHand[3] = deck.draw();
+        }else if (playerHand[3] != null){
+            playerHand[4] = deck.draw();
+        }else if (playerHand[4] != null){
+            specialFive();
+        }
 
     }
     public void stay(){
@@ -71,6 +89,25 @@ public class BlackJack implements Game, GamblingGame{
     }
     public Boolean isLoser(){
 return null;
+    }
+    public Integer checkPlayerHand(){
+        int handValue = 0;
+        for (Integer i = 0; i < playerHand.length; i++){
+            Card currentCard = playerHand[i];
+            handValue += currentCard.getCardValue().getValue();
+        }
+        return null;
+    }
+    public void initialHand(){
+        dealerHand[0]= deck.draw();
+        dealerHand[1]= deck.draw();
+
+        playerHand[0]= deck.draw();
+        playerHand[1]= deck.draw();
+
+    }
+    public void specialFive(){
+        isWinner();
     }
 
     @Override
