5d0e1a89d92c70c8f4848ea55fe36924a7338ec3
MikeGray31 <michaelkgray31@gmail.com>
2019-11-05 17:22:41 -0500

    Bug fixes for Yahtzee Class and more DiceRollTests

--- src/test/java/io/zipcoder/casino/DiceGamesTests/YahtzeeTests/DiceRollTest.java
+++ src/test/java/io/zipcoder/casino/DiceGamesTests/YahtzeeTests/DiceRollTest.java
@@ -3,6 +3,7 @@ package io.zipcoder.casino.DiceGamesTests.YahtzeeTests;
 import io.zipcoder.casino.DiceGames.Dice;
 import io.zipcoder.casino.DiceGames.Yahtzee;
 import io.zipcoder.casino.Player.Player;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -40,5 +41,27 @@ public class DiceRollTest {
         }
         dice[1].setKept(true);
         Assert.assertTrue(dice[1].isKept());
+    }
+
+    @Test
+    public void resetTest(){
+        roller.roll5Dice();
+        Dice[] dice = roller.getMyDice();
+
+        for(int i = 0; i < roller.getMyDice().length; i++){
+            dice[i].setKept(true);
+            Assert.assertTrue(dice[i].isKept());
+        }
+
+        roller.resetDice();
+
+        for(int i = 0; i < roller.getMyDice().length; i++){
+            Assert.assertFalse(dice[i].isKept());
+        }
+    }
+
+    @After
+    public void doThisLast(){
+        
     }
 }
