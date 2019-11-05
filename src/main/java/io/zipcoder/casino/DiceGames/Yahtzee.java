8b4689bd3d21665804e5a1aa913639e172c139d8
Mike Gray <mike@zipcoders-MacBook-Pro.local>
2019-11-04 19:48:53 -0500

    added methods to yahtzee class

--- src/main/java/io/zipcoder/casino/DiceGames/Yahtzee.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Yahtzee.java
@@ -1,7 +1,25 @@
 package io.zipcoder.casino.DiceGames;
 
+import io.zipcoder.casino.Interfaces.Game;
 import io.zipcoder.casino.Player.Player;
 
-public class Yahtzee {
+public class Yahtzee implements Game {
     private Player user;
+
+
+    public void startGame(){
+        int points = 0;
+        for(int i = 0; i < 13; i++) {
+            //rollDice
+            //chooseEvaluation
+        }
+    }
+
+
+    public void promptLeaveGame(){
+
+    }
+    public void displayResults(){
+
+    }
 }
