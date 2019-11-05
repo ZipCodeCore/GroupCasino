0ac26b6692a63c5f438a741474a9106f5c8180c3
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-05 11:49:39 -0500

    Add logic

--- src/main/java/io/zipcoder/casino/DiceGames/Dice.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Dice.java
@@ -4,18 +4,25 @@ import io.zipcoder.casino.Interfaces.GamblingGame;
 
 import java.util.ArrayList;
 
-public abstract class Dice implements GamblingGame {
+public class Dice implements GamblingGame {
 
-    public static ArrayList<Integer> dealerDice = new ArrayList<Integer>(5);
+
     public static ArrayList<Integer> playerDice = new ArrayList<Integer>(5);
-    public static ArrayList<Integer> heldDice = new ArrayList<Integer>(5);
+
     public static Integer numOfDice = 0;
 
+    public Dice(int numOfDice){}
 
-    public Dice(){}
+
 
     public int diceValue(){return 0;}
 
-    public static void createDiceAndroll(int[]numberOfDice){ }
+    public static void roll(int[]numberOfDice){
+
+    }
+
+    public void distributeWinnings(){
+
+    }
 }
 
