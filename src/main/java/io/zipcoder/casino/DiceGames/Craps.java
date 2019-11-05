9910a741577c1446f1bc13b01ec313201d38d5b9
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-04 19:31:43 -0500

    Craps stub

--- src/main/java/io/zipcoder/casino/DiceGames/Craps.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Craps.java
@@ -2,16 +2,19 @@ package io.zipcoder.casino.DiceGames;
 
 public class Craps {
 
-public void   user(){}
-public void diceValues (){}
-public void  currentRolledNumber(){}
-public void targetNumber(){}
-public void startGame(){}
-public int []  roll(int[] , Craps diceValues)(){}
-public void distributeWinnings(){}
-public void evaluateDiceValue (){}
-public void  promptLeaveGame(){}
-public void  displayResults(){}
+    public void user(){}
+    public void diceValues (){}
+    public void  currentRolledNumber(){}
+    public void targetNumber(){}
+    public void startGame(){}
+    public int [] roll (int[] diceValues){
+        return null;
+    }
+
+    public void distributeWinnings(){}
+    public void evaluateDiceValue (){}
+    public void  promptLeaveGame(){}
+    public void  displayResults(){}
 
 
 
