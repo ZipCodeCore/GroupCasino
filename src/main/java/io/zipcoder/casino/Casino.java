3454898ec5d9b2cad8c625cdbb997a57760e3f6e
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-05 10:22:35 -0500

    App class and main menu

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -4,10 +4,13 @@ package io.zipcoder.casino;
 import io.zipcoder.casino.utilities.Console;
 
 public class Casino {
+
     public static void main(String[] args) {
         // write your tests before you start fucking with this
-        Console menu = new Console(System.in,System.out);
 
-        menu.getIntegerInput("Please Select Game");
+        App newCasino = new App();
+        newCasino.App();
+
     }
+
 }
