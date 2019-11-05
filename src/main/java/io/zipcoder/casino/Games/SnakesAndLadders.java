2bc04e20b50eb3194b2089371c0c455d9f284f3d
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-05 10:40:06 -0500

    added snakes and ladders reset into its own method

--- src/main/java/io/zipcoder/casino/Games/SnakesAndLadders.java
+++ src/main/java/io/zipcoder/casino/Games/SnakesAndLadders.java
@@ -23,6 +23,14 @@ public class SnakesAndLadders implements Game {
         approachTable(currentPlayer);
     }
 
+    public void setUpGame(){
+        running = true;
+        currentGame = true;
+        String winner = "";
+        playerPiece.setCurrentPosition(0);
+        aiPiece.setCurrentPosition(0);
+    }
+
     public String startNewGame(){
         Integer playerPosition = playerPiece.getCurrentPosition();
         Integer aiPosition = aiPiece.getCurrentPosition();
@@ -260,15 +268,11 @@ public class SnakesAndLadders implements Game {
 
     @Override
     public void runGame(Player currentPlayer) {
-        running = true;
-        currentGame = true;
-        String winner = "";
-        playerPiece.setCurrentPosition(0);
-        aiPiece.setCurrentPosition(0);
+        setUpGame();
         while (running) {
             console.println("Welcome to Snakes and Ladders, %s!", currentPlayer.getName());
             console.println("In this house, the player always goes first! Step on up!");
-            winner = startNewGame();
+            String winner = startNewGame();
             if (winner.equals("Player")) {
                 console.println("Congratulations! You won!");
             } else if (winner.equals("Ai")) {
