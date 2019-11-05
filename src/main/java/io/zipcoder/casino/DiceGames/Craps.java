60bb62dbc135da19e3dec5fe330c38aa65eae82d
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-05 16:31:19 -0500

    Changes to craps

--- src/main/java/io/zipcoder/casino/DiceGames/Craps.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Craps.java
@@ -1,26 +1,34 @@
 package io.zipcoder.casino.DiceGames;
 
+import io.zipcoder.casino.Interfaces.GamblingGame;
 import io.zipcoder.casino.Interfaces.GamblingPlayer;
+import io.zipcoder.casino.Interfaces.Game;
+import io.zipcoder.casino.utilities.Console;
 
-public class Craps{
+public class Craps implements Game, GamblingGame,GamblingPlayer {
+
+    public Craps crapsGame = new
+
+    private static Console console = new Console(System.in, System.out);
+
     private String[] availableOptionsCrapsNames = {"Roll the Die ","YO Levine the Dancing Queen", "Place Your Bets",
                                                     "Winner Winner Chicken Dinner", "Nina Nina from Pasedena"};
 
     public void user(){}
     public void diceValues (){}
     public void  currentRolledNumber(){}
-    public void targetNumber(){}
+    public void PointNumber(){}
     public void startGame(){}
     public int [] roll (int[] diceValues){
         return null;
     }
-
     public void distributeWinnings(){}
     public void evaluateDiceValue (){}
     public void  promptLeaveGame(){}
     public void  displayResults(){}
 
 
+    public void placeWage() {
 
-
+    }
 }
