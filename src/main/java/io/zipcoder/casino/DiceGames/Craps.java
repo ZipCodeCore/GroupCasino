79ff3bf81f7e74e1c217655eabdac6c2c50d7124
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-04 19:27:12 -0500

    Added methods for craps

---
+++ src/main/java/io/zipcoder/casino/DiceGames/Craps.java
@@ -0,0 +1,19 @@
+package io.zipcoder.casino.DiceGames;
+
+public class Craps {
+
+public void   user(){}
+public void diceValues (){}
+public void  currentRolledNumber(){}
+public void targetNumber(){}
+public void startGame(){}
+public int []  roll(int[] , Craps diceValues)(){}
+public void distributeWinnings(){}
+public void evaluateDiceValue (){}
+public void  promptLeaveGame(){}
+public void  displayResults(){}
+
+
+
+
+}
