daed46c2ce1c2d8a5ee79ebf371488133e1e764c
SandyPham823 <sandydupham@gmail.com>
2019-11-04 23:57:19 -0500

    The opening just got cooler

--- src/main/java/io/zipcoder/casino/Menus/Casino.java
+++ src/main/java/io/zipcoder/casino/Menus/Casino.java
@@ -3,6 +3,7 @@ package io.zipcoder.casino.Menus;
 
 import io.zipcoder.casino.Player;
 import io.zipcoder.casino.Utilities.Console;
+import io.zipcoder.casino.Utilities.Music;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -11,14 +12,41 @@ import java.util.concurrent.TimeUnit;
 
 public class Casino {
 
+
+
     private static Console console = new Console(System.in, System.out);
 
+
     public static void main(String[] args) throws InterruptedException {
+
+        //Starts playing music!
+        Music introMusic = null;
+        try {
+            Music.filePath = "src/music/(Intro) City sound effect 1 - downtown.wav";
+            introMusic = new Music();
+            introMusic.play();
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
+        }
+
         // write your tests before you start fucking with this
         Casino.prologue();
         Player player = Casino.newPlayerDialogue();
 
         MainMenu mainMenu = new MainMenu(player);
+
+        //stops the sound effects
+        try {
+            introMusic.stop();
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+
         mainMenu.displayMenu();
         Casino.displayEnding();
     }
@@ -64,13 +92,15 @@ public class Casino {
 
 
     public static Player newPlayerDialogue() {
+
+
+
         String firstName = console.getInput("((You walk up to the entrance and get greeted by a tall, stocky man with a handlebar mustache))\n" +
                 "\n[TELLER]: What's your first name?\n");
         String lastName = console.getInput("\n[TELLER]: What's your last name? \n");
         int age = console.getInteger("\n[TELLER]: How old are you? \n");
         return new Player(firstName, lastName, age, 0.0);
     }
-
 
 
     public static void displayEnding() {
