05a10a329e7c18f022876da1496197b9d9b1104a
Ben Coon <bencoon@zipcoders-MacBook-Pro-22.local>
2019-11-05 17:27:28 -0500

    Card Services work

--- src/main/java/services/CardService.java
+++ src/main/java/services/CardService.java
@@ -7,25 +7,22 @@ import java.util.Stack;
 
 public class CardService {
 
-    // Declare an instance of Deck of Cards.  Make sure Deck of Cards is a collection type Stack
+    DeckOfCards deckOfCards = new DeckOfCards();// Declare an instance of Deck of Cards.  Make sure Deck of Cards is a collection type Stack
 
 
     public DeckOfCards getDeckOfCards() {
-        //return deckOfCards;   // or something like that
-        return null;
+        return deckOfCards;
     }
 
     public void setDeckOfCards(Stack<Card> deckOfCards) {
-        //this.deckOfCards = deckOfCards;  // I guess
+        this.deckOfCards = deckOfCards;  // I guess
     }
 
     public DeckOfCards shuffle(DeckOfCards deck) {
-        //return Collections.shuffle(deck);
-        return null;
+        return Collections.shuffle(deck);
     }
 
     public Card cardOnTop(DeckOfCards deckOfCards) {
-        //return deckOfCards.pop();    // pop() will not work unless deckOfCards is of collection type Stack
-        return null;
+        return deckOfCards.pop();    // pop() will not work unless deckOfCards is of collection type Stack
     }
 }
