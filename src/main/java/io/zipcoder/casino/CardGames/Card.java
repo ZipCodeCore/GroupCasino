ee05356295d8423e32afda2399abdf945fca8d28
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-11-05 09:00:56 -0500

    add deck of cards

--- src/main/java/io/zipcoder/casino/CardGames/Card.java
+++ src/main/java/io/zipcoder/casino/CardGames/Card.java
@@ -1,4 +1,30 @@
 package io.zipcoder.casino.CardGames;
 
 public class Card {
+
+    private Suit suit;
+    private Rank rank;
+
+
+    public Card(){
+
+    }
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public String toString() {
+
+        return String.format("%s of %s", rank, suit);
+    }
 }
--- src/main/java/io/zipcoder/casino/CardGames/GoFish.java
+++ src/main/java/io/zipcoder/casino/CardGames/GoFish.java
@@ -3,21 +3,28 @@ package io.zipcoder.casino.CardGames;
 import io.zipcoder.casino.Player.Player;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class GoFish {
 
     Player user = new Player();
+    Deck goFishDeck = new Deck(1);
+
     Integer pointAccumulated;
     Integer hasCardQuestion;
-    Deck goFishDeck = new Deck();
 
-    public void startGame(){}
 
-    public boolean askForCard(Player thisPlayer, int cardNumber){}
+    public void startGame(){
+
+    }
+
+    public boolean askForCard(Player thisPlayer, int cardNumber){return false;}
 
     public void tradeCards(ArrayList fromPlayer, ArrayList toPlayer){}
 
-    public void dropCards (Player playerToDrop){}
+    public void dropCards (Player playerToDrop){
+
+    }
 
     public void promptLeaveGame(){}
 
