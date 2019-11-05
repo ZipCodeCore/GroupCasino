101a50ed11a93a711c925e1a5cfb4f5d8d7607cd
Kwamè Warrick <kwame@zipcoders-MacBook-Pro-108.local>
2019-11-05 11:54:11 -0500

    fixed merge conflict

--- src/main/java/io/zipcoder/casino/DiceGames/Dice.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Dice.java
@@ -1,33 +1,9 @@
 package io.zipcoder.casino.DiceGames;
-
-<<<<<<< HEAD
 import io.zipcoder.casino.Interfaces.GamblingGame;
 
 import java.util.ArrayList;
 
-public class Dice implements GamblingGame {
 
-
-    public static ArrayList<Integer> playerDice = new ArrayList<Integer>(5);
-
-    public static Integer numOfDice = 0;
-
-    public Dice(int numOfDice){}
-
-
-
-    public int diceValue(){return 0;}
-
-    public static void roll(int[]numberOfDice){
-
-    }
-
-    public void distributeWinnings(){
-
-    }
-}
-
-=======
 public class Dice {
     private boolean kept;
     private int value;
@@ -58,5 +34,23 @@ public class Dice {
     public String toString(){
         return String.format("Value: %d  : Keeping: %b", value, kept);
     }
+
+    public static ArrayList<Integer> playerDice = new ArrayList<Integer>(5);
+
+    public static Integer numOfDice = 0;
+
+    public Dice(int numOfDice){}
+
+
+
+    public int diceValue(){return 0;}
+
+    public static void roll(int[]numberOfDice){
+
+    }
+
+    public void distributeWinnings(){
+
+    }
 }
->>>>>>> dev
+
