6484dd7e995f6a67c0633e91c6cc3e27f90e3eaa
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-05 18:10:09 -0500

    Go fish 'started'

--- src/main/java/io/zipcoder/casino/Games/GoFish.java
+++ src/main/java/io/zipcoder/casino/Games/GoFish.java
@@ -1,8 +1,23 @@
 package io.zipcoder.casino.Games;
 
+import io.zipcoder.casino.GamePieces.Card;
+import io.zipcoder.casino.GamePieces.Deck;
 import io.zipcoder.casino.Player;
+import io.zipcoder.casino.utilities.Console;
 
 public class GoFish implements Game {
+    Deck deck = new Deck();
+    Console console = new Console(System.in, System.out);
+    Card[] playerHand = new Card[6];
+    Card[] dealerHand = new Card[6];
+    private Player currentPlayer;
+    private boolean currentGame = true;
+    boolean running = true;
+
+
+
+
+
     @Override
     public void approachTable(Player currentPLayer) {
 
