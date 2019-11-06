f2bfb3eba12df8a7042d022d6db1045b56ed69a8
ushanil <ush.gopi@gmail.com>
2019-11-06 10:10:34 -0500

    comitting

--- src/main/java/services/PlayerService.java
+++ src/main/java/services/PlayerService.java
@@ -3,19 +3,36 @@ package services;
 import models.hands.Hand;
 import models.people.players.Player;
 
-public class PlayerService {
 
+public static class PlayerService {
+    Player player1 = new Player();
+    Double balance;
 
-    public static Double getBalance(Player player) {
-        return null;
+    public PlayerService(Player player1) {
+        this.player1 = player1;
+        balance = 0.0;
     }
 
-    public static Double depositMoney(Double depositAmt) {
-        return null;
+    public static Double getBalance(Player player) {
+
+        return player.getBalance();
+    }
+
+    public static Double depositMoney(Double depositAmt)
+    {
+
+        balance = player1.getBalance();
+        balance += depositAmt;
+
+        return balance;
     }
 
     public static Double withdrawMoney(Double withdrawAmt) {
-        return null;
+        Player player1 = new Player();
+        Double balance = player1.getBalance();
+        balance -= withdrawAmt;
+
+        return balance;
     }
 
     public static Hand resetPlayerHands(Player player) {
