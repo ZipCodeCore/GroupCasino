fcdb732f7f4a8df13628484a093f2ec4d017c97b
MannyMonocle <eetumadu@gmail.com>
2019-11-05 19:01:48 -0500

    player wageGood added

--- src/main/java/io/zipcoder/casino/Player/GamblingPlayer.java
+++ src/main/java/io/zipcoder/casino/Player/GamblingPlayer.java
@@ -3,17 +3,18 @@ package io.zipcoder.casino.Player;
 public class GamblingPlayer implements io.zipcoder.casino.Interfaces.GamblingPlayer {
 
     Player gambler;
+    Boolean wageGood;
     Double pot = 0.0;
 
     public GamblingPlayer(Player user){
         this.gambler = user;
     }
 
-    public Boolean placeWage(double wageAmount) {
-        if(gambler.balance < wageAmount) {return false;}
+    public void placeWage(double wageAmount) {
+        if(gambler.balance < wageAmount) {this.wageGood = false;}
         else {gambler.balance -= wageAmount;
             this.pot += wageAmount;
-        return true;}
+        this.wageGood = true;}
     }
 
     public void getWinnings(){
