0f013b9c04d7f2615c2e43b476bc1049d1d633a3
jeanvalentin51 <valentin.j.gheorghe.com>
2019-11-06 19:54:56 -0500

    Add test for trade cards

--- src/test/java/io/zipcoder/casino/CardGames/GoFishTest.java
+++ src/test/java/io/zipcoder/casino/CardGames/GoFishTest.java
@@ -38,6 +38,33 @@ public class GoFishTest {
 
     @Test
     public void tradeCards() {
+        boolean result = false;
+        Player testPlayer = new Player("V","G");
+        GoFish testGame = new GoFish(testPlayer);
+
+        Card firstCard = new Card(Suit.CLUBS,Rank.KING);
+        Card secondCard = new Card(Suit.CLUBS,Rank.ACE);
+
+        ArrayList<Card> giveHand = new ArrayList<>();
+        giveHand.add(firstCard);
+        giveHand.add(secondCard);
+        CardHand handOne = new CardHand(giveHand);
+
+//        ************
+
+        Card cardThree = new Card(Suit.CLUBS,Rank.JACK);
+        Card cardFour = new Card(Suit.CLUBS,Rank.QUEEN);
+
+        ArrayList<Card> receiveHand = new ArrayList<>();
+        receiveHand.add(cardThree);
+        receiveHand.add(cardFour);
+        CardHand handTwo = new CardHand(receiveHand);
+
+        result = testGame.tradeCards(handOne,"KING",handTwo).size() > 0;
+        Assert.assertTrue(result);
+
+        result = testGame.tradeCards(handOne,"TEN",handTwo).size() > 0;
+        Assert.assertFalse(result);
     }
 
     @Test
