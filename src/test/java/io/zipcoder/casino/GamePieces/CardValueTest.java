22d8ba5491ab768b9b65a954cad807f0af251840
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-07 15:22:37 -0500

    tests

---
+++ src/test/java/io/zipcoder/casino/GamePieces/CardValueTest.java
@@ -0,0 +1,17 @@
+package io.zipcoder.casino.GamePieces;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardValueTest {
+
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void fromInt() {
+    }
+}
