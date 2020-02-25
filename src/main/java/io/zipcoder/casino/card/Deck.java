5e48be5a97c052250a406a70d59101134abbc552
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-02-25 14:11:58 -0500

    saving

--- src/main/java/io/zipcoder/casino/card/Deck.java
+++ src/main/java/io/zipcoder/casino/card/Deck.java
@@ -1,5 +1,12 @@
 package io.zipcoder.casino.card;
 
+import java.util.Stack;
+
 public class Deck {
+    Stack<Card> deck;
+
+    public Deck() {
+
+    }
 
 }
--- src/test/java/io/zipcoder/casino/DeckTest.java
+++ src/test/java/io/zipcoder/casino/DeckTest.java
@@ -1,4 +1,17 @@
 package io.zipcoder.casino;
 
+import io.zipcoder.casino.card.Card;
+import io.zipcoder.casino.card.Deck;
+import org.junit.Test;
+
+import java.util.Stack;
+
 public class DeckTest {
+    /*@Test
+    public void testDeck(){
+
+        Stack<Card> deck = new Stack<>();
+
+
+    }*/
 }
