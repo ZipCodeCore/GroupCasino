7f12aa45b2c222b5c97dc2bd096593e3f8c1b508
DavidKellyonGitHub <dave.kelly.purchase@gmail.com>
2020-02-25 18:49:52 -0500

    Deck stubbed, constructor finished

--- src/main/java/io/zipcoder/casino/GameClasses/CardGames/Card.java
+++ src/main/java/io/zipcoder/casino/GameClasses/CardGames/Card.java
@@ -1,4 +1,21 @@
 package io.zipcoder.casino.GameClasses.CardGames;
 
 public class Card {
+    private EnumRank rank;
+    private EnumSuit suit;
+
+    public Card(EnumRank cardRank, EnumSuit cardSuit) {
+        this.rank = cardRank;
+        this.suit = cardSuit;
+    }
+
+    public EnumSuit getSuit() {
+        return suit;
+    }
+
+    public EnumRank getRank() {
+        return rank;
+    }
+
+
 }
