96e3aa237da2593dea3584cea3cd5c4f1e43cf2c
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-02-25 17:03:36 -0500

    saving

--- src/main/java/io/zipcoder/casino/card/Deck.java
+++ src/main/java/io/zipcoder/casino/card/Deck.java
@@ -1,10 +1,12 @@
 package io.zipcoder.casino.card;
 
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Deck {
     Stack<Card> deck;
+    ArrayList<Card> deckToShuffle;
 
     public Deck() {
         this.deck = new Stack<>();
@@ -34,6 +36,35 @@ public class Deck {
         return deck;
     }
 
-    
+    public boolean isEmpty() {
+        return deck.isEmpty();
+    }
+
+    public Card popCard() {
+        return deck.pop();
+    }
+
+    public void printDeck() {
+        System.out.println(deck);
+    }
+
+    public void stackToArray() {
+        deckToShuffle = new ArrayList(deck);
+    }
+
+    public ArrayList<Card> getTempArrayList() {
+        return deckToShuffle;
+    }
+
+    public void shuffleDeck() {
+        Card tempToSwapTo;
+        for (int i = 0; i < 52; i++) {
+            int random = (int)(Math.random() * 52);
+            for (int j = 0; j <= random; j++) {
+                tempToSwapTo = deck.get(j);
+
+            }
+        }
+    }
 }
 
--- src/test/java/io/zipcoder/casino/DeckTest.java
+++ src/test/java/io/zipcoder/casino/DeckTest.java
@@ -4,6 +4,8 @@ import io.zipcoder.casino.card.Card;
 import io.zipcoder.casino.card.Deck;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Stack;
 import java.util.logging.Logger;
 
@@ -14,7 +16,7 @@ public class DeckTest {
 
 
     @Test
-    public void testCreateDeck(){
+    public void testCreateDeck() {
         Deck deck = new Deck();
 
         Integer expected = 52;
@@ -26,4 +28,50 @@ public class DeckTest {
         LOGGER.info("\n" + actual);
     }
 
+    @Test
+    public void testGetDeck() {
+        Deck deck = new Deck();
+        deck.createDeck();
+
+        Assert.assertFalse(deck.isEmpty());
+        Assert.assertTrue(!deck.isEmpty());
+    }
+
+    @Test
+    public void testPopCard() {
+        Deck deck = new Deck();
+        deck.createDeck();
+
+        deck.popCard();
+        Integer actualDeckSize = deck.getDeck().size();
+        Integer remainingCards = 51;
+        Assert.assertEquals(remainingCards, actualDeckSize);
+    }
+
+    @Test
+    public void testPrintDeck() {
+        Deck deck = new Deck();
+        deck.createDeck();
+        deck.printDeck();
+    }
+
+    @Test
+    public void testShuffleDeck() {
+        Deck deck = new Deck();
+        deck.createDeck();
+        //deck.shuffleDeck();
+        deck.printDeck();
+    }
+
+    @Test
+    public void testStackToArray() {
+        Deck deck = new Deck();
+        deck.createDeck();
+        deck.stackToArray();
+
+        Integer actual = deck.getTempArrayList().size();
+        Integer expected = 52;
+
+        Assert.assertEquals(expected, actual);
+    }
 }
