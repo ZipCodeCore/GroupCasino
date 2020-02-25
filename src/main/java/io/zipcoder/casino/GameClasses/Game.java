f677f83602994c441682c8a268020f2a0c8219be
Jeremy McCray <jmccray@zipcodes-MacBook-Pro-2.local>
2020-02-25 16:52:36 -0500

    gamblingGame and game interfaces compleated

--- src/main/java/io/zipcoder/casino/GameClasses/Game.java
+++ src/main/java/io/zipcoder/casino/GameClasses/Game.java
@@ -1,4 +1,16 @@
 package io.zipcoder.casino.GameClasses;
 
+import io.zipcoder.casino.Player.Player;
+
 public interface Game {
+
+    public void startGame();
+
+    public Boolean quitGame();
+
+    public Boolean win();
+
+    public Boolean playAgain();
+
+    public Player playerBuilder();
 }
