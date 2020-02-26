43290dc3a4f106dc37e44d28fe0a5fe942594b40
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-26 11:21:34 -0500

    Card has been fully tested

--- src/main/java/io/zipcoder/casino/Card.java
+++ src/main/java/io/zipcoder/casino/Card.java
@@ -3,54 +3,74 @@
 
 package io.zipcoder.casino;
 
-public class Card implements Comparable<Card> {
-    public enum Rank {
+public class Card implements Comparable<Card>
+{
+    public enum Rank
+    {
         DEUCE(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9),
         TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
         private Integer rankValue;
-        public final Integer getRankValue() {
+        public final Integer getRankValue()
+        {
             return rankValue;
         }
         Rank(final int rv) {
             rankValue = rv;
         }
-        public Integer CardValue() {
+        public Integer cardValue() {
             return rankValue;
         }
     }
-    public enum Suit {
+
+    public enum Suit
+    {
         CLUBS(2), DIAMONDS(3), HEARTS(4), SPADES(1);
         private Integer suitValue;
-        public final Integer getSuitValue() {
+        public final Integer getSuitValue()
+        {
             return suitValue;
         }
-        Suit(final int sv) {
+        Suit(final int sv)
+        {
             suitValue = sv;
         }
-        public Integer SuitValue() {
+        public Integer SuitValue()
+        {
             return suitValue;
         }
     }
+
     private final Rank rank;
     private final Suit suit;
-    protected Card(final Rank rank, final Suit suit) {
+    protected Card(final Rank rank, final Suit suit)
+    {
         this.rank = rank;
         this.suit = suit;
     }
-    public Rank rank() {
+
+    public Rank rank()
+    {
         return rank;
     }
-    public Suit suit() {
+
+    public Suit suit()
+    {
         return suit;
     }
+
     @Override
-    public String toString() {
+    public String toString()
+    {
         return rank + " of " + suit + "(" + cardValue() + ")";
     }
-    public Integer cardValue() {
+
+    public Integer cardValue()
+    {
         return rank.getRankValue() * suit.getSuitValue();
     }
-    public int compareTo(Card o) {
+
+    public int compareTo(Card o)
+    {
         return -this.cardValue().compareTo(o.cardValue());
     }
 }
--- src/test/java/io/zipcoder/casino/CardTest.java
+++ src/test/java/io/zipcoder/casino/CardTest.java
@@ -14,37 +14,41 @@ public class CardTest
     public void cardValues()
     {
         // GIVEN
+
         Card testAce = new Card(ACE, SPADES);
         Card.Rank expectedRank = ACE;
         Card.Suit expectedSuit = SPADES;
         Integer expectedVal = 11;
+        Integer expectedSuitValue = 1;
 
         // WHEN
 
         Card.Rank actualRank = testAce.rank();
         Card.Suit actualSuit = testAce.suit();
         Integer actualVal = testAce.rank().getRankValue();
+        Integer actualSuitValue = testAce.suit().getSuitValue();
 
         // THEN
 
         assertEquals(expectedRank, actualRank);
         assertEquals(expectedSuit, actualSuit);
         assertEquals(expectedVal, actualVal);
+        assertEquals(expectedSuitValue, actualSuitValue);
     }
 
     @Test
     public void cardRankingJack()
     {
         // GIVEN
-        Card testAce = new Card(JACK, DIAMONDS);
+        Card testJack = new Card(JACK, DIAMONDS);
         Card.Rank expectedRank = JACK;
         Card.Suit expectedSuit = DIAMONDS;
         Integer expectedVal = 10;
 
         // WHEN
 
-        Card.Rank actualRank = testAce.rank();
-        Card.Suit actualSuit = testAce.suit();
+        Card.Rank actualRank = testJack.rank();
+        Card.Suit actualSuit = testJack.suit();
         Integer actualVal = actualRank.getRankValue();
 
         // THEN
@@ -58,21 +62,72 @@ public class CardTest
     public void cardRankingDeuce()
     {
         // GIVEN
-        Card testAce = new Card(DEUCE, CLUBS);
+        Card testDeuce = new Card(DEUCE, CLUBS);
         Card.Rank expectedRank = DEUCE;
         Card.Suit expectedSuit = CLUBS;
         Integer expectedRankVal = 2;
 
         // WHEN
 
-        Card.Rank actualRank = testAce.rank();
-        Card.Suit actualSuit = testAce.suit();
-        Integer actualVal = actualRank.getRankValue();
+        Card.Rank actualRank = testDeuce.rank();
+        Card.Suit actualSuit = testDeuce.suit();
+        Integer actualVal = actualRank.cardValue();
 
         // THEN
 
         assertEquals(expectedRank, actualRank);
         assertEquals(expectedSuit, actualSuit);
         assertEquals(expectedRankVal, actualVal);
+    }
+
+    @Test
+    public void cardString()
+    {
+        // GIVEN
+
+        Card testAce = new Card(ACE, SPADES);
+        Card testQueen = new Card(QUEEN, HEARTS);
+        Card testTen = new Card(TEN, DIAMONDS);
+        String expectedAce = "ACE of SPADES(11)";
+        String expectedQueen = "QUEEN of HEARTS(40)";
+        String expectedTen = "TEN of DIAMONDS(30)";
+
+        // WHEN
+
+        String actualAce = testAce.toString();
+        String actualQueen = testQueen.toString();
+        String actualTen = testTen.toString();
+
+        // THEN
+
+        assertEquals(expectedAce, actualAce);
+        assertEquals(expectedQueen, actualQueen);
+        assertEquals(expectedTen, actualTen);
+    }
+
+    @Test
+    public void comparingCards()
+    {
+        // GIVEN
+
+        Card testAce = new Card(ACE, SPADES);
+        Card testJack = new Card(JACK, CLUBS);
+        Card testEightSpades = new Card(EIGHT, SPADES);
+        Card testEightHearts = new Card(EIGHT, HEARTS);
+
+        Integer expectedFirstCompare = 1;
+        Integer expectedSecondCompare = -1;
+        Integer expectedThirdCompare = 1;
+        // WHEN
+
+        Integer actualFirstCompare = testAce.compareTo(testJack);
+        Integer actualSecondCompare = testJack.compareTo(testAce);
+        Integer actualThirdCompare = testEightSpades.compareTo(testEightHearts);
+
+        // THEN
+
+        assertEquals(expectedFirstCompare, actualFirstCompare);
+        assertEquals(expectedSecondCompare, actualSecondCompare);
+        assertEquals(expectedFirstCompare, actualThirdCompare);
     }
 }
