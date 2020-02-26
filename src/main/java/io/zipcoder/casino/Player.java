43912e066ae12a98bb403a74d2131d1f9fd8b3ab
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-02-26 10:00:33 -0500

    Fields in Player class

--- src/main/java/io/zipcoder/casino/Player.java
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -1,4 +1,14 @@
 package io.zipcoder.casino;
 
-public class Player {
+public abstract class Player
+{
+    public String name;
+    public Integer gamblingWallet;
+
+    public Player(String name, Integer gamblingWallet)
+    {
+        this.name = name;
+        this.gamblingWallet = gamblingWallet;
+    }
+
 }
