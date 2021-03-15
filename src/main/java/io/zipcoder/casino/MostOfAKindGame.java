b4685c3726ccb4e8826cfed30c147199e3dd480e
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-15 16:40:45 -0400

    added checkwinner boolean

--- src/main/java/io/zipcoder/casino/MostOfAKindGame.java
+++ src/main/java/io/zipcoder/casino/MostOfAKindGame.java
@@ -16,4 +16,22 @@ public class MostOfAKindGame extends DiceGame {
     public int getPot() {
         return pot;
     }
+
+    public int getDealerCount() {
+        return dealerCount;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    public boolean checkWinner() {
+        boolean doIWin = false;
+        if (getDealerCount() > getPlayerCount()) {
+            return doIWin;
+        } else if (getPlayerCount() > getDealerCount()) {
+            doIWin = true;
+        }
+        return doIWin;
+    }
 }
