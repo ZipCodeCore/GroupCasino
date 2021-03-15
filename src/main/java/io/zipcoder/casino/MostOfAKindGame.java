66721935b9a210918d15a4f80c1998fc3309da84
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-15 16:06:52 -0400

    added fields to MostOfAKind

--- src/main/java/io/zipcoder/casino/MostOfAKindGame.java
+++ src/main/java/io/zipcoder/casino/MostOfAKindGame.java
@@ -2,7 +2,13 @@ package io.zipcoder.casino;
 
 import io.zipcoder.casino.DiceGame;
 
+import java.util.ArrayList;
+
 public class MostOfAKindGame extends DiceGame {
+    ArrayList<Integer> dealerHand;
+    int dealerCount;
+    int playerCount;
+
     public void addToPot(int numOfChips) {
         pot += numOfChips;
     }
