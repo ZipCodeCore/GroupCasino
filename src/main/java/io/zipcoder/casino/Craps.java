9246c79ad607547688937f90a60719e9ebe8a894
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-15 23:08:56 -0400

    craps should be fully written, passing all tests

--- src/main/java/io/zipcoder/casino/Craps.java
+++ src/main/java/io/zipcoder/casino/Craps.java
@@ -1,8 +1,12 @@
 package io.zipcoder.casino;
 
 public class Craps extends DiceGame {
-    int gameRound;
-    String passOrNot = "";
+    int gameRound = 1;
+    String betStatus;
+    Integer[] roundOnePass = new Integer[]{7, 11};
+    Integer[] roundOneNotPass = new Integer[]{2, 3, 12};
+    Integer[] rollAgain = new Integer[]{4, 5, 6, 8, 9, 10};
+    int pointer;
 
     public void addToPot(int numOfChips) {
         pot += numOfChips;
@@ -10,5 +14,103 @@ public class Craps extends DiceGame {
 
     public int getPot() {
         return pot;
+    }
+
+
+    //1: player wins, 2: dealer wins, 3: move to next round
+    public int checkWinnerRoundOne(int sumOfDice) {
+        int outcome = 0;
+        if (getBetStatus().equalsIgnoreCase("Pass")) {
+            for (int i : roundOnePass) {
+                if (sumOfDice == i) {
+                    outcome = 1;
+                }
+            }
+            for (int i : roundOneNotPass) {
+                if (sumOfDice == i) {
+                    outcome = 2;
+                }
+            }
+            for (int i : rollAgain) {
+                if (sumOfDice == i) {
+                    outcome = 3;
+                }
+            }
+        } else if (getBetStatus().equalsIgnoreCase("Not Pass")) {
+            for (int i : roundOnePass) {
+                if (sumOfDice == i) {
+                    outcome = 2;
+                }
+            }
+            for (int i : roundOneNotPass) {
+                if (sumOfDice == i) {
+                    outcome = 1;
+                }
+            }
+            for (int i : rollAgain) {
+                if (sumOfDice == i) {
+                    outcome = 3;
+                }
+            }
+        }
+        return outcome;
+    }
+
+
+
+    //1: player wins, 2: player loses, 3: roll again
+    public int checkWinnerRoundTwo(int sumOfDice) {
+        int outcome;
+        if (getBetStatus().equalsIgnoreCase("Pass")) {
+            if (sumOfDice == getPointer()) {
+                outcome = 1;
+            } else if (sumOfDice == 7) {
+                outcome = 2;
+            } else {
+                outcome = 3;
+            }
+        } else {
+            //catches "NotPass" status
+            if (sumOfDice == 7) {
+                outcome = 1;
+            } else if (sumOfDice == getPointer()) {
+                outcome = 2;
+            } else {
+                outcome = 3;
+            }
+        }
+        return outcome;
+    }
+
+    public int sumOfDice() {
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            sum += rollDice();
+        }
+        return sum;
+    }
+
+    public void setGameRound(int round) {
+        gameRound = round;
+    }
+
+    public int getGameRound() {
+        return gameRound;
+    }
+
+    public void setPointer(int pointer) {
+        this.pointer = pointer;
+    }
+
+    public int getPointer() {
+        return this.pointer;
+    }
+
+    public void setBetStatus(String betStatus) {
+        this.betStatus = betStatus;
+    }
+
+    public String getBetStatus() {
+        return this.betStatus;
     }
 }
--- src/test/java/io/zipcoder/casino/CrapsTest.java
+++ src/test/java/io/zipcoder/casino/CrapsTest.java
@@ -5,14 +5,15 @@ import org.junit.Test;
 
 public class CrapsTest {
 
+    //SuperClass tests
     @Test
     public void getPot() {
         //Given
-        MostOfAKindGame yahtzee = new MostOfAKindGame();
+        Craps craps = new Craps();
         int expected = 0;
 
         //When
-        int actual = yahtzee.getPot();
+        int actual = craps.getPot();
 
         //Then
         Assert.assertEquals(expected, actual);
@@ -21,12 +22,12 @@ public class CrapsTest {
     @Test
     public void addToPot() {
         //Given
-        MostOfAKindGame yahtzee = new MostOfAKindGame();
+        Craps craps = new Craps();
         int expected = 5;
 
         //When
-        yahtzee.addToPot(5);
-        int actual = yahtzee.getPot();
+        craps.addToPot(5);
+        int actual = craps.getPot();
 
         //Then
         Assert.assertEquals(expected, actual);
@@ -35,13 +36,13 @@ public class CrapsTest {
     @Test
     public void clearPot() {
         //Given
-        MostOfAKindGame yahtzee = new MostOfAKindGame();
+        Craps craps = new Craps();
         int expected = 0;
-        yahtzee.addToPot(7);
-        yahtzee.clearPot();
+        craps.addToPot(7);
+        craps.clearPot();
 
         //When
-        int actual = yahtzee.getPot();
+        int actual = craps.getPot();
 
         //Then
         Assert.assertEquals(expected, actual);
@@ -50,25 +51,259 @@ public class CrapsTest {
     @Test
     public void rollDice() {
         //Given
-        MostOfAKindGame yahtzee = new MostOfAKindGame();
+        Craps craps = new Craps();
 
         //When
-        int result = yahtzee.rollDice();
+        int result = craps.rollDice();
 
         //Then
         Assert.assertTrue(result > 0 && result < 7);
     }
 
+
+    //Unique to Craps
     @Test
-    public void checkWinner() {
+    public void setGameRound() {
         //Given
-        MostOfAKindGame yahtzee = new MostOfAKindGame();
-        boolean expected = false;
+        Craps craps = new Craps();
+        int expected = 1;
 
         //When
-        boolean actual = yahtzee.checkWinner();
+        craps.setGameRound(1);
+        int actual = craps.getGameRound();
 
         //Then
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void setGetPointer() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 8;
+
+        //When
+        craps.setPointer(8);
+        int actual = craps.getPointer();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setGetBetStatus() {
+        //Given
+        Craps craps = new Craps();
+        String expected = "Pass";
+
+        //When
+        craps.setBetStatus("Pass");
+        String actual = craps.getBetStatus();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sumOfDice() {
+        //Given
+        Craps craps = new Craps();
+
+        //When
+        int actual = craps.sumOfDice();
+
+        //Then
+        Assert.assertTrue(actual > 1 && actual < 13);
+    }
+
+    //Round One Pass Outcomes
+    @Test
+    public void playerWinsRoundOnePass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 1;
+        craps.setBetStatus("Pass");
+
+        //When
+        int dieSum = 7;
+        int actual = craps.checkWinnerRoundOne(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerLosesRoundOnePass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 2;
+        craps.setBetStatus("Pass");
+
+        //When
+        int dieSum = 2;
+        int actual = craps.checkWinnerRoundOne(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerRollsAgainPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 3;
+        craps.setBetStatus("Pass");
+
+        //When
+        int dieSum = 4;
+        int actual = craps.checkWinnerRoundOne(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    //Round One Not Pass Outcomes
+    @Test
+    public void playerWinsRoundOneNotPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 1;
+        craps.setBetStatus("Not Pass");
+
+        //When
+        int dieSum = 2;
+        int actual = craps.checkWinnerRoundOne(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerLosesRoundOneNotPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 2;
+        craps.setBetStatus("Not Pass");
+
+        //When
+        int dieSum = 7;
+        int actual = craps.checkWinnerRoundOne(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerRollsAgainRoundOneNotPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 3;
+        craps.setBetStatus("Not Pass");
+
+        //When
+        int dieSum = 9;
+        int actual = craps.checkWinnerRoundOne(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    //Round Two Pass Outcomes
+    @Test
+    public void playerWinsRoundTwoPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 1;
+        craps.setBetStatus("Pass");
+        craps.setPointer(10);
+
+        //When
+        int dieSum = 10;
+        int actual = craps.checkWinnerRoundTwo(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerLosesRoundTwoPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 2;
+        craps.setBetStatus("Pass");
+        craps.setPointer(10);
+
+        //When
+        int dieSum = 7;
+        int actual = craps.checkWinnerRoundTwo(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerRollsAgainRoundTwoPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 3;
+        craps.setBetStatus("Pass");
+        craps.setPointer(10);
+
+        //When
+        int dieSum = 4;
+        int actual = craps.checkWinnerRoundTwo(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    //Round Two Not Pass Outcomes
+    @Test
+    public void playerWinsRoundTwoNotPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 1;
+        craps.setBetStatus("Not Pass");
+        craps.setPointer(10);
+
+        //When
+        int dieSum = 7;
+        int actual = craps.checkWinnerRoundTwo(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerLosesRoundTwoNotPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 2;
+        craps.setBetStatus("Not Pass");
+        craps.setPointer(10);
+
+        //When
+        int dieSum = 10;
+        int actual = craps.checkWinnerRoundTwo(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void playerRollsAgainRoundTwoNotPass() {
+        //Given
+        Craps craps = new Craps();
+        int expected = 3;
+        craps.setBetStatus("Not Pass");
+        craps.setPointer(10);
+
+        //When
+        int dieSum = 11;
+        int actual = craps.checkWinnerRoundTwo(dieSum);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
 }
