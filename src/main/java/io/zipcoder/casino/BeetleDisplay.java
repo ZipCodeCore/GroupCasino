0c2e48202fb10f33e6cd5185f5f23123db633c29
r-m-hufford <ryan@rmhufford.com>
2021-03-16 12:59:40 -0400

    Beetle added. Beetle Display started

---
+++ src/main/java/io/zipcoder/casino/BeetleDisplay.java
@@ -0,0 +1,40 @@
+package io.zipcoder.casino;
+
+public class BeetleDisplay extends Display{
+
+    public void rules() {
+        String rules = "Draw the beetle before your opponent and win big!\nThe rules are simple:\nEach side of the die correspond to a part of the beetle." +
+                "\n If you roll a number, you get to draw that part\nIf you roll the same number again, you do not get to add to your beetle." +
+                "\nBetween each roll you will add to your wager.\n The first person to draw a beetle keeps the purse.";
+    }
+
+    public void drawingMade() {
+
+    }
+
+    public void drewLeg() {
+
+    }
+
+    public void drewBody() {
+
+    }
+
+    public void drewHead() {
+
+    }
+
+    public void yourBugIsComplete() {
+
+    }
+
+    public void opponentsBugisComplete() {
+
+    }
+
+    public void repeatedRollResult() {
+
+    }
+
+
+}
