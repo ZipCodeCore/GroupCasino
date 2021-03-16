70bc2bce6645b35e7feb644a6d3fd0d3a366126e
Jbanksalpha <78812718+Jbanksalpha@users.noreply.github.com>
2021-03-16 13:45:46 -0400

    Made changes to the card games

---
+++ src/main/java/io/zipcoder/casino/utilities/Blackjack.java
@@ -0,0 +1,4 @@
+package io.zipcoder.casino.utilities;
+
+public class Blackjack {
+}
---
+++ src/main/java/io/zipcoder/casino/utilities/Value.java
@@ -0,0 +1,5 @@
+package io.zipcoder.casino.utilities;
+
+public enum Value {
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
+}
