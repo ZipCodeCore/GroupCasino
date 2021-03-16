f109c06c017003dd1230d32c116a50532563c36f
r-m-hufford <ryan@rmhufford.com>
2021-03-16 15:14:24 -0400

    game interface. dice game abstract class.

---
+++ src/main/java/io/zipcoder/casino/DiceGame.java
@@ -0,0 +1,18 @@
+package io.zipcoder.casino;
+
+import java.util.Random;
+
+abstract class DiceGame {
+
+    public Integer diceRollSum(int i) {
+        Random random = new Random();
+        Integer thisToss = 0;
+
+        while (i >= 1) {
+            int randomGen = (random.nextInt(6) + 1);
+            thisToss += randomGen;
+            i--;
+        }
+        return thisToss;
+    }
+}
