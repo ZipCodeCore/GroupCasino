c45f963822a5c21ae3b614e30b06fa237b685dd3
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-16 14:14:13 -0400

    starting screens. ascii art class if I can figure it out, if not, I'll scrap it

---
+++ src/main/java/io/zipcoder/casino/AsciiArt.java
@@ -0,0 +1,9 @@
+package io.zipcoder.casino;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class AsciiArt {
+
+
+}
---
+++ src/main/java/io/zipcoder/casino/Displays.java
@@ -0,0 +1,74 @@
+package io.zipcoder.casino;
+
+import java.util.Scanner;
+
+public class Displays {
+
+    public static String getStringInput() {
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static void print(String output, Object... args) {
+        System.out.printf(output, args);
+    }
+
+    public static void println(String output, Object... args) {
+        print(output + "\n", args);
+    }
+
+    public void welcomeScreen() {
+        println("                             ");
+        println("        $$  $$               ");
+        println("     $$$$$$$$$$$$$            ");
+        println("    $$$$$$$$$$$$$$$           ");
+        println("   $$$   $$  $$ $$$$         ");
+        println("   $$$   $$  $$  $$$         ");
+        println("   $$$   $$  $$               ");
+        println("    $$$$ $$  $$               ");
+        println("     $$$$$$$$$$$$$            ");
+        println("      $$$$$$$$$$$$$           ");
+        println("         $$  $$ $$$$          ");
+        println("         $$  $$   $$$         ");
+        println("    $$$  $$  $$   $$$         ");
+        println("    $$$$ $$  $$ $$$$          ");
+        println("     $$$$$$$$$$$$$$$          ");
+        println("      $$$$$$$$$$$$            ");
+        println("         $$  $$               ");
+        println("                              ");
+        println(" Welcome to The ZCW Casino!!! ");
+        println("------------------------------");
+        println("     Enter Casino: 00         ");
+        println("     Leave Casino: 99         ");
+    }
+
+    public void invalidEntryWelcomeScreen() {
+        println("                              ");
+        println("  Invalid Entry. Try again    ");
+        println("------------------------------");
+        println("     Enter Casino: 00         ");
+        println("     Leave Casino: 99         ");
+    }
+
+    public void goodbyeCasinoScreen() {
+        println("                              ");
+        println("   Have a wonderful day!      ");
+        println("     Come again soon!         ");
+        println("                              ");
+    }
+
+    public void casinoLobbyScreen() {
+        println("                              ");
+        println("     May the odds be          ");
+        println("    ever in your favor!       ");
+        println("------------------------------");
+        println("    Teller Window:   1        ");
+        println("   *GoFish Room:     2        ");
+        println("    BlackJack Room:  3        ");
+        println("    Craps Room:      4        ");
+        println("    Poker Dice Room: 5        ");
+        println("                              ");
+        println(" *Don't need chips to play    ");
+    }
+}
