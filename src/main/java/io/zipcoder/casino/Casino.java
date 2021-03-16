ddd5a05892032f951c497c22371fa1c50ef7d773
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-16 16:53:21 -0400

    took stuff out of casino that I was testing

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -5,12 +5,10 @@ import io.zipcoder.casino.utilities.Console;
 
 public class Casino {
     public static void main(String[] args) {
-        Casino casino = new Casino();
-        casino.run();
+
     }
 
     public void run() {
-        CrapsScreens crapsScreens = new CrapsScreens();
-        crapsScreens.crapsWelcomeScreen();
+
     }
 }
