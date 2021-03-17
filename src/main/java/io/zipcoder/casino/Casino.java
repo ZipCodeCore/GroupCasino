91be8bbe5dea6569c4d4f2f14c09aa4cd86bd2a9
hazel <liam.k.becker@gmail.com>
2021-03-17 13:38:42 -0400

    Checkpointing

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -17,8 +17,5 @@ import java.util.Scanner;
 public class Casino {
     public static void main(String[] args) {
         // write your tests before you start
-        Display display = new Display();
-        System.out.println(display.helloMessage());
-        display.getUserInput("Welcome whats your name?");
     }
 }
--- src/main/java/io/zipcoder/casino/CrapsDisplay.java
+++ src/main/java/io/zipcoder/casino/CrapsDisplay.java
@@ -2,24 +2,24 @@ package io.zipcoder.casino;
 
 public class CrapsDisplay extends Display {
 
+    public void initalRulesAsk(){
+        setPrintCurrentDisplay("Welcome! Would you like the rules first?");
+    }
+
     public void printRulesMenu1(){
-        setPrintCurrentDisplay("Welcome to Craps!\nIn Craps, the 'shooter' rolls two dice repeatedly.\nTheir first roll, the 'come out roll,' has three potential outcomes.\nA 7 or 11 total is a pass. A 2, 3, or 12 is craps. Anything else is a point.\nYou can bet 'pass' (the shooter rolls a pass) or 'don't pass' (the shooter rolls craps) for 1 to 1 payout.\nThe shooter keeps rolling until a point is set. On a point outcome, the number rolled is saved.\nNow, the shooter can 'come' by hitting the point outcome again, or 'seven out,' ending the round by rolling a 7.\nYou can bet 'come' (shooter hits point before sevening out) or 'don't come' (vice versa) for 1 to 1 payout.");
+        setPrintCurrentDisplay("Welcome to Craps!\nIn Craps, the 'shooter' rolls two dice repeatedly.\nTheir first roll, the 'come out roll,' has three potential outcomes.\nA 7 or 11 total is a pass. A 2, 3, or 12 is craps. Anything else is a point.\nYou can bet 'pass' (the shooter rolls a pass) or 'don't pass' (the shooter rolls craps) for 1 to 1 payout.\nThe shooter keeps rolling until a point is set. On a point outcome, the number rolled is saved.\nNow, the shooter can 'come' by hitting the point outcome again, or 'seven out,' ending the round by rolling a 7.\nYou can bet 'come' (shooter hits point before sevening out) or 'don't come' (vice versa) for 1 to 1 payout.\n");
     }
 
     public void printRulesMenuMid(){
-        setPrintCurrentDisplay("Type rules2 for more technical rules.");
+        setPrintCurrentDisplay("There are more technical rules. Do you want them?");
     }
 
     public void printRulesMenu2(){
-        setPrintCurrentDisplay("There are a number of extra bets you can make, called propositions.\nOne-roll propositions are a bet that the next roll will land on a specific number, with variable payouts.\n6 or 8- 9 to 1 payout. 4 or 10- 7 to 1. 2 or 12- 30 to 1. 3 or 11- 15 to 1. Craps- 7 to 1. 7- 4 to 1.\nField bets are the final one-roll bet. Rolling 3, 4, 9, 10, or 11 pays 1 to 1. Rolling 2 or 12 pays 2 to 1.\nPlace bets can be performed once the point is known. You're betting that a specific different point option will be rolled- this has 1 to 1 payout.\nThat's every bet in Craps! Have fun!");
+        setPrintCurrentDisplay("There are a number of extra bets you can make, called propositions.\nOne-roll propositions are a bet that the next roll will land on a specific number, with variable payouts.\n6 or 8- 9 to 1 payout. 4 or 10- 7 to 1. 2 or 12- 30 to 1. 3 or 11- 15 to 1. Craps- 7 to 1. 7- 4 to 1.\nThe field bet is the final one-roll bet. Rolling 3, 4, 9, 10, or 11 pays 1 to 1. Rolling 2 or 12 pays 2 to 1.\nPlace bets can be performed once the point is known. You're betting that a specific different point option will be rolled- this has 1 to 1 payout.\nThat's every bet in Craps! Have fun!\n");
     }
 
-    public void printOptionsMenu1(){
-        setPrintCurrentDisplay("You can make a pass bet, a don't pass bet, or a proposition.");
-    }
-
-    public void printOptionsMenu2(){
-        setPrintCurrentDisplay("You can make a come bet, a don't come bet, or a proposition.");
+    public void readyToPlay(){
+        setPrintCurrentDisplay("Alright! Let's play Craps!");
     }
 
     public void printShooterScores(){
@@ -55,7 +55,7 @@ public class CrapsDisplay extends Display {
     }
 
     public void printSetTypeOfBet(){
-        setPrintCurrentDisplay("What kind of bet do you want to make? You can also type 'options' or 'help'.");
+        setPrintCurrentDisplay("What kind of bet do you want to make? You can also type 1 for help.");
     }
 
     public void printComeOutRoll(){
