48dab138bfcf78bae34470ce5519b1ac2be8af31
r-m-hufford <ryan@rmhufford.com>
2021-03-17 08:04:26 -0400

    drew a beetle. Player class.

--- src/main/java/io/zipcoder/casino/BeetleDisplay.java
+++ src/main/java/io/zipcoder/casino/BeetleDisplay.java
@@ -1,39 +1,71 @@
 package io.zipcoder.casino;
 
-public class BeetleDisplay extends Display{
+import io.zipcoder.casino.utilities.Console;
+
+import java.util.Arrays;
+
+public class BeetleDisplay extends Display {
+
+    String[][] beetle = new String[][]{{"\\","0","/"},
+            {"⎛","|","⎞"},
+            {"⎝","|","⎠"},
+            {"/","⏝","\\"}
+    };
+
+    public BeetleDisplay() {
+    }
 
     public void rules() {
-        currentDisplaySP("Draw the beetle before your opponent and win big!\nThe rules are simple:\nEach side of the die correspond to a part of the beetle." +
-                "\n If you roll a number, you get to draw that part\nIf you roll the same number again, you do not get to add to your beetle." +
-                "\nBetween each roll you will add to your wager.\n The first person to draw a beetle keeps the purse.\n" +
+        setPrintCurrentDisplay("Draw the beetle before your opponent and win big!\nThe rules are simple:\nEach side of the die corresponds to a part of the beetle." +
+                "\nIf you roll a number, you get to draw that part.\nIf you roll the same number again, you do not get to add to your beetle." +
+                "\nBetween each roll you will add to your wager.\nThe first person to draw a beetle keeps the purse." +
                 "\nIf both players complete their beetle on the same set of rolls the wagers are returned." +
-                " What to draw for each roll:" +
+                "\n\nWhat to draw for each roll:" +
                 "\n1 - head\n2 - body\n3 - left front leg\n4 - right front leg\n5 - left back leg\n6 - right back leg");
     }
 
     public void drewLeg() {
-        currentDisplaySP("You added a leg to your beetle!");
+        setPrintCurrentDisplay("You added a leg to your beetle!");
     }
 
     public void drewBody() {
-        currentDisplaySP("You drew your beetle's body!");
+        setPrintCurrentDisplay("You drew your beetle's body!");
     }
 
     public void drewHead() {
-        currentDisplaySP("You drew your beetle's head!");
+        setPrintCurrentDisplay("You drew your beetle's head!");
     }
 
     public void yourBugIsComplete() {
-        currentDisplaySP("Your beetle is complete!");
+        setPrintCurrentDisplay("Your beetle is complete!");
     }
 
     public void opponentsBugisComplete() {
-        currentDisplaySP("Your opponents beetle is complete.");
+        setPrintCurrentDisplay("Your opponents beetle is complete.");
     }
 
-    public void bothBeetlesComplete(){currentDisplaySP("You both drew beetles. The game is a tie.");}
+    public void bothBeetlesComplete() {
+        setPrintCurrentDisplay("You both drew beetles. The game is a tie.");
+    }
 
     public void repeatedRollResult() {
-        currentDisplaySP("You already rolled that number.");
+        setPrintCurrentDisplay("You already rolled that number.");
+    }
+
+    //Beetle Body Parts
+    public void getBeetleComplete() {
+        //System.out.println(Arrays.deepToString(beetle));
+        /*for (String[] row : beetle) {
+            System.out.println(Arrays.deepToString(row));*/
+        for (int i = 0; i < beetle.length; i++) {
+            for (int j = 0; j < beetle[i].length; j++) {
+                System.out.print(beetle[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void getCurrentBeetle() {
+
     }
 }
--- src/main/java/io/zipcoder/casino/CrapsDisplay.java
+++ src/main/java/io/zipcoder/casino/CrapsDisplay.java
@@ -3,63 +3,63 @@ package io.zipcoder.casino;
 public class CrapsDisplay extends Display {
 
     public void printRulesMenu1(){
-        currentDisplaySP("Welcome to Craps!\nIn Craps, the 'shooter' rolls two dice repeatedly.\nTheir first roll, the 'come out roll,' has three potential outcomes.\nA 7 or 11 total is a pass. A 2, 3, or 12 is craps. Anything else is a point.\nYou can bet 'pass' (the shooter rolls a pass) or 'don't pass' (the shooter rolls craps) for 1 to 1 payout.\nThe shooter keeps rolling until a point is set. On a point outcome, the number rolled is saved.\nNow, the shooter can 'come' by hitting the point outcome again, or 'seven out,' ending the round by rolling a 7.\nYou can bet 'come' (shooter hits point before sevening out) or 'don't come' (vice versa) for 1 to 1 payout.");
+        setPrintCurrentDisplay("Welcome to Craps!\nIn Craps, the 'shooter' rolls two dice repeatedly.\nTheir first roll, the 'come out roll,' has three potential outcomes.\nA 7 or 11 total is a pass. A 2, 3, or 12 is craps. Anything else is a point.\nYou can bet 'pass' (the shooter rolls a pass) or 'don't pass' (the shooter rolls craps) for 1 to 1 payout.\nThe shooter keeps rolling until a point is set. On a point outcome, the number rolled is saved.\nNow, the shooter can 'come' by hitting the point outcome again, or 'seven out,' ending the round by rolling a 7.\nYou can bet 'come' (shooter hits point before sevening out) or 'don't come' (vice versa) for 1 to 1 payout.");
     }
 
     public void printRulesMenuMid(){
-        currentDisplaySP("Type rules2 for more technical rules.");
+        setPrintCurrentDisplay("Type rules2 for more technical rules.");
     }
 
     public void printRulesMenu2(){
-        currentDisplaySP("There are a number of extra bets you can make, called propositions.\nOne-roll propositions are a bet that the next roll will land on a specific number, with variable payouts.\n6 or 8- 9 to 1 payout. 4 or 10- 7 to 1. 2 or 12- 30 to 1. 3 or 11- 15 to 1. Craps- 7 to 1. 7- 4 to 1.\nField bets are the final one-roll bet. Rolling 3, 4, 9, 10, or 11 pays 1 to 1. Rolling 2 or 12 pays 2 to 1.\nPlace bets can be performed once the point is known. You're betting that a specific different point option will be rolled- this has 1 to 1 payout.\nThat's every bet in Craps! Have fun!");
+        setPrintCurrentDisplay("There are a number of extra bets you can make, called propositions.\nOne-roll propositions are a bet that the next roll will land on a specific number, with variable payouts.\n6 or 8- 9 to 1 payout. 4 or 10- 7 to 1. 2 or 12- 30 to 1. 3 or 11- 15 to 1. Craps- 7 to 1. 7- 4 to 1.\nField bets are the final one-roll bet. Rolling 3, 4, 9, 10, or 11 pays 1 to 1. Rolling 2 or 12 pays 2 to 1.\nPlace bets can be performed once the point is known. You're betting that a specific different point option will be rolled- this has 1 to 1 payout.\nThat's every bet in Craps! Have fun!");
     }
 
     public void printOptionsMenu1(){
-        currentDisplaySP("You can make a pass bet, a don't pass bet, or a proposition.");
+        setPrintCurrentDisplay("You can make a pass bet, a don't pass bet, or a proposition.");
     }
 
     public void printOptionsMenu2(){
-        currentDisplaySP("You can make a come bet, a don't come bet, or a proposition.");
+        setPrintCurrentDisplay("You can make a come bet, a don't come bet, or a proposition.");
     }
 
     public void printShooterScores(){
-        currentDisplaySP("The shooter got the point!");
+        setPrintCurrentDisplay("The shooter got the point!");
     }
 
     public void printShooterSevenedOut(){
-        currentDisplaySP("The shooter sevened out...");
+        setPrintCurrentDisplay("The shooter sevened out...");
     }
 
     public void printPush(){
-        currentDisplaySP("Your bet is a push.");
+        setPrintCurrentDisplay("Your bet is a push.");
     }
 
     public void printOneRolLBetLoss(){
-        currentDisplaySP("Your one-roll proposition failed...");
+        setPrintCurrentDisplay("Your one-roll proposition failed...");
     }
 
     public void printOneRollBetWin(){
-        currentDisplaySP("You won your one-roll proposition!");
+        setPrintCurrentDisplay("You won your one-roll proposition!");
     }
 
     public void printNatural(){
-        currentDisplaySP("It's a natural!");
+        setPrintCurrentDisplay("It's a natural!");
     }
 
     public void printCraps(){
-        currentDisplaySP("It's craps!");
+        setPrintCurrentDisplay("It's craps!");
     }
 
     public void printShooterRolled(){
-        currentDisplaySP("The shooter rolled a " + 5);
+        setPrintCurrentDisplay("The shooter rolled a " + 5);
     }
 
     public void printSetTypeOfBet(){
-        currentDisplaySP("What kind of bet do you want to make? You can also type 'options' or 'help'.");
+        setPrintCurrentDisplay("What kind of bet do you want to make? You can also type 'options' or 'help'.");
     }
 
     public void printComeOutRoll(){
-        currentDisplaySP("Here's the come out roll...");
+        setPrintCurrentDisplay("Here's the come out roll...");
     }
 }
 
