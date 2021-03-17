1bad8ccc9d30f2759125c3c7f62a1f10856765a7
mike <mike@zipcodes-mbp-5.lan>
2021-03-16 20:40:42 -0400

    started  on dice and craps

--- src/main/java/io/zipcoder/casino/DiceGames/Craps/Craps.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Craps/Craps.java
@@ -1,4 +1,14 @@
 package io.zipcoder.casino.DiceGames.Craps;
 
-public class Craps {
+import io.zipcoder.casino.DiceGames.Dices.Dice;
+import io.zipcoder.casino.DiceGames.Dices.DiceGame;
+import io.zipcoder.casino.Games.Game;
+
+import java.util.ArrayList;
+
+public class Craps extends DiceGame implements Game {
+
+    private ArrayList<Dice> thrownDice;
+    private Integer sumOfDice;
+    private Integer pointNumber;
 }
--- src/main/java/io/zipcoder/casino/DiceGames/Dices/Dice.java
+++ src/main/java/io/zipcoder/casino/DiceGames/Dices/Dice.java
@@ -1,7 +1,19 @@
 package io.zipcoder.casino.DiceGames.Dices;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Dice {
 
     private int numOfDice;
     private int result;
+
+    public Dice(int numOfDice){
+        this.numOfDice = numOfDice;
+    }
+
+    public int rollDice(){
+        Random random = new Random();
+        return random.nextInt(6) + 1;
+    }
 }
