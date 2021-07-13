b510dfaecff67dd09aafa25974aa4ebc8f919a93
charnaexw <charnaexw@gmail.com>
2021-07-13 16:26:21 -0400

    Tests

---
+++ src/test/java/com/github/zipcodewilmington/casino/games/roulette/CrapsTest.java
@@ -0,0 +1,8 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import com.github.zipcodewilmington.casino.games.CrapsGame.Dice;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CrapsTest {}
+
---
+++ src/test/java/com/github/zipcodewilmington/casino/games/roulette/DiceTest.java
@@ -0,0 +1,21 @@
+package com.github.zipcodewilmington.casino.games.roulette;
+
+import com.github.zipcodewilmington.casino.games.CrapsGame.Dice;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DiceTest {
+
+    @Test
+    public void diceTest(){
+        //given
+        Dice dice = new Dice();
+        int expected=0;
+        //when
+        int actual = dice.rollingTheDice();
+
+        //then
+        Assert.assertNotEquals(expected, actual);
+
+    }
+}
