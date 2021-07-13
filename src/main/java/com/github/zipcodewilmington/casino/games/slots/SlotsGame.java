9481226d51864d8a5395a15012ec3aa2ad1f7587
Raymond <raymond@zipcoder.local>
2021-07-13 15:55:52 -0400

    Testing pull

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,7 +1,32 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
+import com.fasterxml.jackson.databind.util.ISO8601Utils;
+import com.github.zipcodewilmington.casino.GameInterface;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by leon on 7/21/2020.
  */
-public class SlotsGame {
+public class SlotsGame implements GameInterface {
+Random generator = new Random();
+Scanner scanner = new Scanner(System.in);
+int slot1, slot2, slot3;
+
+    @Override
+    public void add(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void remove(PlayerInterface player) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
---
+++ src/test/java/com/github/zipcodewilmington/SlotsTest.java
@@ -0,0 +1,32 @@
+package com.github.zipcodewilmington;
+
+import org.junit.Test;
+
+public class SlotsTest {
+    @Test
+    public void winnerTest(){
+        //given
+
+        //when
+
+        //then
+    }
+    @Test
+    public void loserTest(){
+        //given
+
+        //when
+
+        //then
+    }
+    @Test
+    public void insertMoney(){
+        //given
+
+        //when
+
+        //then
+    }
+
+
+}
