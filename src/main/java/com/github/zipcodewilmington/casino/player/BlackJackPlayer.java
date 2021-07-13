973a003d4926959e6d65b586d11826eb8a421b43
jgiroso <jrgiroso@gmail.com>
2021-07-13 18:32:23 -0400

    added Player class and BlackJackPlayer methods

--- src/main/java/com/github/zipcodewilmington/casino/player/BlackJackPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/player/BlackJackPlayer.java
@@ -1,4 +1,41 @@
 package com.github.zipcodewilmington.casino.player;
 
+import com.github.zipcodewilmington.casino.card.Deck;
+import com.github.zipcodewilmington.casino.card.Hand;
+import com.github.zipcodewilmington.casino.games.Blackjack;
+
 public class BlackJackPlayer {
+    private Hand hand = new Hand();
+    private Deck deck = new Deck();
+    private Blackjack blackjack = new Blackjack();
+    private Player player;
+
+    public BlackJackPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Hand getHand() {
+        return hand.getHand();
+    }
+
+    public Player getPlayer() {
+        return this.player;
+    }
+
+    public String hit() {
+        String topCard = deck.pop();
+        String newCard = "";
+        for (int i = 0; i < hand.size(); i++) {
+             if(newCard != hand(i)) {
+                 newCard = topCard;
+             }
+        }
+        return newCard;
+    }
+
+    public String stay() {
+        return blackJack.cardSum;
+    }
+
+
 }
---
+++ src/main/java/com/github/zipcodewilmington/casino/player/Player.java
@@ -0,0 +1,27 @@
+package com.github.zipcodewilmington.casino.player;
+
+public class Player {
+    private String name;
+    private int balance;
+
+    public Player(String name) {
+        this.name = name;
+        this.balance = 1000;
+    }
+    public String getPlayerName() {
+        return name;
+    }
+
+    public void setPlayerName(String name) {
+        this.name = name;
+    }
+
+    public int getPlayerBalance() {
+        return balance;
+    }
+
+    public void setPlayerBalance(int balance) {
+        this.balance = balance;
+    }
+
+}
