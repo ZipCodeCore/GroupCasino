412c509f82d74b3b4e090a4e041cc3b888752b18
ZachSinger <32113115+ZachSinger@users.noreply.github.com>
2021-07-13 20:48:40 -0400

    CasinoAccount and CasinoAccountManager.java (#19)
    Co-authored-by: Zach <zach@zipcodes-MacBook-Pro-6.local>

--- src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
+++ src/main/java/com/github/zipcodewilmington/casino/CasinoAccount.java
@@ -1,9 +1,38 @@
 package com.github.zipcodewilmington.casino;
 
+import com.github.zipcodewilmington.Casino;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 7/21/2020.
  * `ArcadeAccount` is registered for each user of the `Arcade`.
  * The `ArcadeAccount` is used to log into the system to select a `Game` to play.
  */
 public class CasinoAccount {
+    private String password;
+    private String accountName;
+
+
+    public CasinoAccount(String accountName, String accountPassword){
+        this.accountName = accountName;
+        this.password = accountPassword;
+    }
+
+
+
+    /*
+    public void setPassword(String password){
+        if(this.validPass()){
+            if(this.confirmPass()){
+                this.password = password;
+            } else {
+
+            }
+        }
+    }
+
+     */
+
 }
