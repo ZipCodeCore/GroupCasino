e67c9dd62c9ea1189e702ed6770336952d48adea
Nick <nick@zipcodes-mbp-78.myfiosgateway.com>
2021-07-14 13:18:56 -0400

    Pushing for pull

--- src/test/java/com/github/zipcodewilmington/BlackJackGameTest.java
+++ src/test/java/com/github/zipcodewilmington/BlackJackGameTest.java
@@ -1,5 +1,7 @@
 package com.github.zipcodewilmington;
 
+import com.github.zipcodewilmington.casino.Player;
+import com.github.zipcodewilmington.casino.PlayerInterface;
 import com.github.zipcodewilmington.casino.games.blackjack.BlackJackGame;
 import org.junit.Test;
 
@@ -7,6 +9,7 @@ public class BlackJackGameTest {
 
     @Test
     public void runTest () {
+        Player player = new Player("Roger", 5000);
         BlackJackGame bj = new BlackJackGame();
 
         Integer userInput = 1;
