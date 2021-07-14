22c31a7559e7f8f50d73b13a109a32e5900a91cd
Nick <nick@zipcodes-mbp-78.myfiosgateway.com>
2021-07-14 19:05:24 -0400

    (feat:black-jack) cleaned the code

--- src/test/java/com/github/zipcodewilmington/BlackJackGameTest.java
+++ src/test/java/com/github/zipcodewilmington/BlackJackGameTest.java
@@ -2,8 +2,12 @@ package com.github.zipcodewilmington;
 
 import com.github.zipcodewilmington.casino.Player;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.games.blackjack.BlackJack;
 import com.github.zipcodewilmington.casino.games.blackjack.BlackJackGame;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class BlackJackGameTest {
 
@@ -14,6 +18,21 @@ public class BlackJackGameTest {
 
         Integer userInput = 1;
         bj.run();
+
+    }
+
+    @Test
+    public void splitPlayer () {
+        BlackJackGame bjg = new BlackJackGame();
+        BlackJack bj = new BlackJack();
+
+        List<Integer> input = Arrays.asList(10, 10);
+        bj.setPlayersHand(input);
+        System.out.println(bj.getPlayersHand());
+        System.out.println(bj.playersCurrentValue());
+
+
+        bjg.splitPlayer();
 
     }
 }
