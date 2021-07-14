8759e2f4022cf7a092d0ec66126ebf40a7879f11
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-13 20:59:50 -0400

    added pig tests & updated pig class

--- src/main/java/com/github/zipcodewilmington/casino/games/Pig.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Pig.java
@@ -1,4 +1,48 @@
 package com.github.zipcodewilmington.casino.games;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Pig {
+
+  private final int numOfDice = 1;
+  private int dieValue;
+  private int rollCounter;
+  private int turnScore;
+  private int pOneTotal;
+  private int pTwoTotal;
+  private char rollAgainAnswer;
+  private char startTurn;
+  public Scanner input;
+
+  public Pig() {
+    Scanner input = new Scanner(System.in);
+  }
+
+  //will open with a welcome screen
+    //get player 2
+    //the rules
+    //s to start
+
+    public void pig(){
+
+    }
+
+    public void playPig(){
+
+    }
+
+    public int rollDie(){
+      Random randomNumber = new Random();
+      dieValue = randomNumber.nextInt(6);
+        if (dieValue == 0){
+            rollDie();
+        }
+      System.out.println("You rolled a " + dieValue);
+      return dieValue;
+    }
+
+
+
+
 }
---
+++ src/test/java/com/github/zipcodewilmington/PigTests.java
@@ -0,0 +1,17 @@
+package com.github.zipcodewilmington;
+
+import com.github.zipcodewilmington.casino.games.Pig;
+import org.junit.jupiter.api.Test;
+
+public class PigTests {
+    private Pig testPig;
+
+    @Test
+    public void testRollDie(){
+
+
+    }
+
+
+
+}
