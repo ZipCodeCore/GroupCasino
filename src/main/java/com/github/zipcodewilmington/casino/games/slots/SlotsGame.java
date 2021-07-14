895135d1ca183ca519361df234bde9a8c9ee8e07
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-07-14 00:53:13 -0400

    finished getBetSelections

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -3,11 +3,13 @@ package com.github.zipcodewilmington.casino.games.slots;
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
+import java.util.Scanner;
+
 /**
  * Created by Nathan on 7/12/21
  */
 public class SlotsGame implements GameInterface {
-
+    private Integer playerBetAmount;
 
     @Override
     public void add(PlayerInterface player) {
@@ -22,11 +24,54 @@ public class SlotsGame implements GameInterface {
     @Override
     public void run() {
         Slots slotMachine = new Slots();
-        //Integer[] bets = takeBet();
+        getBetAmount();
+        Integer[] selectedBets = getBetSelections();
         slotMachine.spinSlots();
-        //slotMachine.compareBetVsWinningLines();
+        String[] betResults =  slotMachine.compareBetVsWinningLines(selectedBets);
+        calculateMultiplier(betResults);
+
+    }
 
 
+
+    private void getBetAmount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How much you do want to bet?");
+        Integer input = scanner.nextInt();
+        playerBetAmount = input;
+    }
+
+    private Integer[] getBetSelections() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many lines do you want to bet on?");
+        Integer numberOfLines = scanner.nextInt();
+
+        System.out.println(
+        "************************************************************************\n" +
+        "**                 Select the lines you want to bet on!               **\n" +
+        "**   1. Top Horizontal  2. Middle Horizontal   3. Bottom Horizontal   **\n" +
+        "**   4. Left Vertical    5. Middle Vertical      6. Right Vertical    **\n" +
+        "**           7. Down Diagonal              8. Up Diagonal             **\n" +
+        "************************************************************************");
+        Integer count = 0;
+        Integer[] selectedLines = new Integer[numberOfLines];
+        while (count < numberOfLines){
+            System.out.println("Select your line #" + (count + 1));
+            selectedLines[count] = scanner.nextInt();
+            count++;
+        }
+        return selectedLines;
+    }
+
+    private void calculateMultiplier(String[] betResults) {
+        Integer count = 0;
+        for(String outcome: betResults){
+            if(outcome.equals("Win")){
+                count++;
+            } else {
+                count--;
+            }
+        }
     }
 
     @Override
