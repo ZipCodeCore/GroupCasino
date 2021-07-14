b4a405e80196db63ac75e66ce52fcf92dac16ef8
Jeremy <jeremysabina@gmail.com>
2021-07-14 15:12:40 -0400

    Made card and Deck

--- src/main/java/com/github/zipcodewilmington/casino/objects/Card.java
+++ src/main/java/com/github/zipcodewilmington/casino/objects/Card.java
@@ -1,4 +1,25 @@
 package com.github.zipcodewilmington.casino.objects;
 
 public class Card {
-}
+    private final CardRank rank;
+    private final CardSuit suit;
+
+    private final String[] rankNames = {"Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace",
+            "Joker"};
+    private final String[] suitNames = {"Clubs", "Diamonds", "Hearts", "Spades",
+            "Black", "Red"};
+
+    public Card(CardRank rank, CardSuit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public CardRank getRank() {
+        return rank;
+    }
+
+    public CardSuit getSuit() {
+        return suit;
+    }
+    }
