87702565b9914101778174e132e8ad46a0741f05
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-14 13:32:03 -0400

    starting over pig 4

--- src/main/java/com/github/zipcodewilmington/casino/games/Pig.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Pig.java
@@ -20,47 +20,73 @@ public class Pig {
   public Scanner input = new Scanner(System.in);
 
 
-  public void welcomeToPig(){
+  public void welcomeToPig() {
     //welcome screen
     //log player 2 in or create account
     //the rules
   }
 
- public String currentStateOfTheGame(){
-    return "It is Player " +
 
- }
-
-
-  public void playerTurn(){
-    System.out.println("Please enter 'r' to roll or 'h' to hold");
+  public void playerTurn(int turnScore) {
+    //while (rollCounter <= 5)
+      System.out.println("Please enter 'r' to roll or 'h' to hold");
     input.nextLine();
     rollAnswer = input.nextLine();
-    if(rollAnswer == "r"){
+    if (rollAnswer == "r") {
       dieValue = ThreadLocalRandom.current().nextInt(1, 7);
-    } if (dieValue == 1){
+      if (dieValue == 1) {
         System.out.println("You rolled a 1; your turn is over and any banked points have been lost.");
         System.out.println("Next player enter 'r' to roll the die!");
         input.nextLine();
         startTurn = input.nextLine();
-    }else{
-      System.out.println("You rolled a " + dieValue);
-      turnScore += dieValue;
-      rollCounter += 1;
+      } else {
+        System.out.println("You rolled a " + dieValue);
+        turnScore += dieValue;
+        rollCounter += 1;
+        playerTurn(turnScore);
+      }
+    } else {
+      if (currentTurn == 1) {
+        pOneTotal += turnScore;
+      } else {
+        pTwoTotal += turnScore;
+      }
+      System.out.println("Player " + currentTurn + "earned " + turnScore + " points.");
     }
-
   }
 
+  public String currentStateOfTheGame(){
+        return "It is Player " + currentTurn + "'s turn. \n Player 1 has " +
+                pOneTotal + " points. \n Player 2 has " + pTwoTotal + " points.";
+    }
 
+  public void switchingPlayers(){
+    System.out.println(currentStateOfTheGame());
+    playerTurn(0);
+    currentTurn = 2;
+    System.out.println("It's Player 2's turn.");
+    playerTurn(0);
+    System.out.println("It's Player 1's turn");
+  }
 
+  public boolean getWinner(){
+    if (pOneTotal == 100){
+      System.out.println("Player 1 won!");
+    }else if(pTwoTotal == 100) {
+      System.out.println("Player 2 won!");
+    }
+    return false;
+  }
 
+  public Pig(){
+    currentTurn = 1;
+    pOneTotal = 0;
+    pTwoTotal = 0;
 
-
-
-
-
-
-
+    while (!getWinner()){
+      switchingPlayers();
+    }
+  }
 
   }
 
