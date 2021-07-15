d6db5cd17199603808e0eb1a42ac9a80e060bb3b
Rex <rex@zipcodes-mbp-51.lan>
2021-07-15 17:59:34 -0400

    War in progress

--- src/main/java/com/github/zipcodewilmington/casino/card/Hand.java
+++ src/main/java/com/github/zipcodewilmington/casino/card/Hand.java
@@ -1,18 +1,25 @@
 package com.github.zipcodewilmington.casino.card;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Hand {
-private ArrayList<Card> hand;
+    private ArrayList<Card> hand;
+    private List<Card> warCards;
+    private Hand warHand;
 
     public Hand() {
 
         hand = new ArrayList<>();
-
+        this.warCards = new ArrayList<Card>();
     }
 
     public void setHand(ArrayList<Card> hand) {
         this.hand = hand;
+    }
+
+    public void setWarHand(Hand warHand) {
+        this.warHand = warHand;
     }
 
     public ArrayList<Card> getHand() {
@@ -32,4 +39,35 @@ private ArrayList<Card> hand;
         hand.add(i);
     }
 
+    public void putCardOnTop(Card warCard) { //add card to hand (deck)
+        warCards.add(warCard);
+    }
+
+    public void putCardOnBottom(Card warCard) { //add card to bottom of hand (deck)
+        warCards.add(0, warCard);
+    }
+
+    public void wonCardsToBottom(Card warCard) { //winner puts won cards to bottom of deck
+        warCards.add(0, warCard);
+    }
+
+    public int handSize() {
+        return warCards.size();
+    }
+
+    public Card playTopCard() { //play top card of hand (deck)
+        return warCards.remove(handSize() - 1 );
+    }
+
+    public void addCardsToTop(List<Card> warHand) { //add all cards to hand (deck) array
+        this.warCards.addAll(warHand);
+    }
+
+    public List<Card> takeCards(int takeCards) {
+        List<Card> playedCards = new ArrayList<>();
+        for (int i = 0; i < takeCards; i++) {
+            playedCards.add(this.warCards.remove(this.handSize() - 1 ));
+        }
+        return playedCards;
+    }
 }
