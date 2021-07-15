407a573e584a921941e082a9c99ce99a12818352
sitarameez <sitarablossom@gmail.com>
2021-07-15 00:14:43 -0400

    done for the night

---
+++ src/test/java/com/github/zipcodewilmington/casino/games/CardGame/BlackJackTest.java
@@ -0,0 +1,38 @@
+package com.github.zipcodewilmington.casino.games.CardGame;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BlackJackTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPlayer() {
+    }
+
+    @Test
+    void playBlackjack() {
+    }
+
+    @Test
+    void checkBust() {
+    }
+
+    @Test
+    void checkHandValue() {
+    }
+
+    @Test
+    void add() {
+    }
+}
--- src/test/java/com/github/zipcodewilmington/casino/games/CardGame/CasinoWarTest.java
+++ src/test/java/com/github/zipcodewilmington/casino/games/CardGame/CasinoWarTest.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington.casino.games.CardGame;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,14 +20,24 @@ class CasinoWarTest {
 
     @Test
     void playCasinoWarHands() {
+        Deck warDeck = new Deck(1);
+        CardHand playerHand;
+        CardHand dealerHand;
+        Card card = new Card();
+
+        playerHand = new CardHand(warDeck.dealCards(1));
+        dealerHand = new CardHand(warDeck.dealCards(1));
+        Integer playHand =  playerHand.userHand.get(0).faceValueOfCard.getCardIntegerValue();
+        Integer dealHand =  dealerHand.userHand.get(0).faceValueOfCard.getCardIntegerValue();
+        System.out.println("player Hand:"+playHand);
+        System.out.println("deal Hand:"+dealHand);
+        Assert.assertTrue(true);
     }
 
     @Test
-    void playerWins() {
-        CardHand playerHand;
-        CardHand dealerHand;
-        Deck warDeck = new Deck();
-        playerHand = new CardHand(warDeck.dealCards(1));
-        dealerHand = new CardHand(warDeck.dealCards(1));
+    void testDetermineWinner() {
+
+
+
     }
 }
