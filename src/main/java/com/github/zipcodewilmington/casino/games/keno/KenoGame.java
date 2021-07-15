b0080ceb145a8b4df9ecfa47c2944bfd3d17d851
Nisha <nisha@zipcodes-mbp-8.myfiosgateway.com>
2021-07-14 21:53:32 -0400

    added some stuff

--- src/main/java/com/github/zipcodewilmington/casino/games/keno/KenoGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/keno/KenoGame.java
@@ -1,18 +1,23 @@
 package com.github.zipcodewilmington.casino.games.keno;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class KenoGame {
 
-    private ArrayList<Integer> numbers;  //list that holds numbers to guess from
-    private ArrayList<Integer> winNumbers;  //list that holds the winning numbers
+    private ArrayList<Integer> numbers = new ArrayList<>();  //list that holds numbers to guess from
+    private ArrayList<Integer> winNumbers = new ArrayList<>();  //list that holds the winning numbers
 
     public KenoGame(ArrayList<Integer> numbers,ArrayList<Integer> winNumbers){
         this.numbers =numbers;
         this.winNumbers = winNumbers;
+    }
+
+    public KenoGame() {
+
     }
 
     public ArrayList<Integer> getNumbers() {
@@ -36,8 +41,8 @@ public class KenoGame {
     }
 
     public ArrayList<Integer> setKenoGame(){
-        //array of answers to guess
-        numbers = new ArrayList<Integer>();
+        //create the array of answers to guess from
+
         for(int i = 1; i <= 80; i++){
             numbers.add(i);
         }
@@ -45,7 +50,7 @@ public class KenoGame {
     }
     //generate winning numbers to play
     public ArrayList<Integer> generateNumbers(){
-        winNumbers = new ArrayList<Integer>();
+
         while (winNumbers.size() < 20){
             int winNum = (int)(Math.random() * 80 + 1);
             if (winNumbers.indexOf(winNum) == -1){
@@ -55,6 +60,32 @@ public class KenoGame {
         return winNumbers;
     }
 
+    //check whether the number is unique or not
+    public boolean isUnique(int number,int[] array){
+        for (int i = 0; i < array.length; i++){
+            if (number ==array[i]){
+                return false;
+            }
+        }
+        return true;
+    }
 
-    
+    // generate the numbers for the computer. numbers are generated randomly
+    public int[] getComputerNum(){
+        int[] computerNum = new int[20];
+        Random r = new Random();
+        int randomNumber = 0;
+
+        for (int i = 0; i < computerNum.length; i++){
+            randomNumber = r.nextInt(80) + 1;
+            if(isUnique(randomNumber,computerNum)){
+                computerNum[i] = randomNumber;
+            }else {
+                i++;
+            }
+        }
+        return computerNum;
+
+    }
+
 }
