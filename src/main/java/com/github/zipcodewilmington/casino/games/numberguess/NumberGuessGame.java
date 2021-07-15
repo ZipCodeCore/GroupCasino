1b703cab26709865d1db7cc5ec3b3cb988d4516f
Nathan <nathan@zipcodes-MacBook-Pro-9.local>
2021-07-15 12:54:00 -0400

    NumberGuessGame fleshed out

--- src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/numberguess/NumberGuessGame.java
@@ -28,32 +28,76 @@ public class NumberGuessGame implements GameInterface {
         Scanner scanner = new Scanner(System.in);
         //print welcome
         printWelcome();
-        //print instruction for game
-        System.out.println("Guess the right number between 0 and 20 to double your bets!\nFeeling lucky??\n\n Increase the difficulty and increase your earnings!");
-        //print difficulty levels and ask user to choose
-        setDifficultyLevel();
-        //print player's current account balance
-        System.out.println("Your current account balance is:   " + this.currentPlayer.getArcadeAccount().getAccountBalance());
-        //ask for a bet amount
-        takeBet();
-        //subtract bet amount from player's account
-        this.currentPlayer.getArcadeAccount().alterAccountBalance(betAmount);
-        //ask player to chose number from 0 to difficulty lvl
-        takeGuess();
-        //generate actual number
-        pickRandomNumber();
-        //compare difference
 
-        //compute winnings
 
-        //print winnings
+        Boolean quitGame = false;
+        while(!quitGame) {
 
-        //add winnings to player's account
+            //print instruction for game
+            printInstructions();
+            System.out.println("Guess the right number between 0 and 20 to double your bets!\n\n      But WAIT!  Feeling lucky??\n\nIncrease the difficulty and increase your earnings!");
 
+            //print difficulty levels and ask user to choose
+            setDifficultyLevel();
+            //set multiplier
+            setMultiplier(maxNumber);
+            //print player's current account balance
+            System.out.println("Your current account balance is:   $" + this.currentPlayer.getArcadeAccount().getAccountBalance());
+            //ask for a bet amount
+            takeBet();
+            //subtract bet amount from player's account
+            subtractBetFromBalance(betAmount);
+            //ask player to chose number from 0 to difficulty lvl
+            takeGuess();
+            //generate actual number
+            pickRandomNumber();
+            //compare difference
+            Integer guessRange = getGuessRange(guessNumber, randomNumber);
+            //calculate amount player won
+            Integer payout = calculatePayout(multiplier, guessRange, betAmount);
+            //print winnings
+            System.out.println("You won:   $"+payout);
+            //add winnings to player's account
+            addMoneyToBalance(currentPlayer,payout);
+            //ask if player wants to play again
+            System.out.println("\u001B[36mWould you like to play again?\n" +
+                    "1. Yes   2. No");
+            Integer input = scanner.nextInt();
+            if(input == 2){
+                quitGame = true;
+            }
+        }
+    }
+
+    private void printInstructions() {
+    }
+
+    private Integer calculatePayout(Integer multiplier, Integer guessRange,Integer betAmount) {
+        Integer payout;
+        Double percent = 0.0;
+        Double multiplierAsDouble = multiplier.doubleValue();
+        if (guessRange.equals(0)){
+            percent = multiplierAsDouble * 1;
+        } else if(guessRange <= 10){
+            percent = multiplierAsDouble * 0.8;
+        } else if(guessRange <= 25){
+            percent = multiplierAsDouble * 0.6;
+        } else if(guessRange <= 35){
+            percent = multiplierAsDouble * 0.4;
+        } else if(guessRange >= 36){
+            percent = multiplierAsDouble * 0.0;
+        }
+
+        Double payoutAsDouble = percent * betAmount;
+        payout = payoutAsDouble.intValue();
+
+        return payout;
     }
 
     private void pickRandomNumber() {
-        this.randomNumber = (int) ((Math.random() * (maxNumber - 0)) + 0);
+        Integer random = (int) ((Math.random() * (maxNumber - 0)) + 0);
+        this.randomNumber = random;
+
     }
 
     private void takeGuess() {
@@ -69,13 +113,14 @@ public class NumberGuessGame implements GameInterface {
 
     private void printWelcome() {
         //TODO
+        System.out.println("WELCOME WELCOME WELCOME!!!!!");
     }
 
     private void setDifficultyLevel() {
         System.out.println("1. Start max at 20 and go for double.\n" +
                 "2. Change max to 30 and Triple your bets!\n" +
-                "3.Change max to 40 and QUADRUPLE your bets! OH MY!\n" +
-                "4.Change max to 50! SEND IT! TO THE MOON!");
+                "3. Change max to 40 and QUADRUPLE your bets! OH MY!\n" +
+                "4. Change max to 50! SEND IT! TO THE MOON!");
         setMaxNumber(scanner.nextInt());
     }
 
@@ -83,12 +128,6 @@ public class NumberGuessGame implements GameInterface {
     public Integer getGuessRange(Integer guessedNumber, Integer actualNumber){
         return guessRangePercentage(Math.abs(guessedNumber - actualNumber));
     }
-
-
-    //Player can set difficulty level to get high yield
-    //Take this return - reward if guess is within 10%
-    //The closer to 0%, the higher reward
-    //10% = money back, 8% = 1.1
 
     public Integer guessRangePercentage(Integer range){
         Double percent =  ((range.doubleValue() / this.maxNumber) * 100);
@@ -102,6 +141,19 @@ public class NumberGuessGame implements GameInterface {
         this.maxNumber = max;
     }
 
+    public void setMultiplier(Integer difficultyLvl) {
+        switch(difficultyLvl){
+            case 20:
+                this.multiplier = 2;
+            case 30:
+                this.multiplier = 3;
+            case 40:
+                this.multiplier = 4;
+            case 50:
+                this.multiplier = 5;
+        }
+    }
+
     @Override
     public void add(PlayerInterface player) {
         this.currentPlayer = player;
@@ -113,7 +165,6 @@ public class NumberGuessGame implements GameInterface {
     }
 
 
-
     @Override
     public Integer calculateWinnings(Integer multiplier, Integer betAmount) {
         return null;
@@ -121,11 +172,11 @@ public class NumberGuessGame implements GameInterface {
 
     @Override
     public void subtractBetFromBalance(Integer betAmount) {
-
+        this.currentPlayer.getArcadeAccount().alterAccountBalance((betAmount * -1));
     }
 
     @Override
     public void addMoneyToBalance(PlayerInterface Player, Integer winnings) {
-
+        Player.getArcadeAccount().alterAccountBalance(winnings);
     }
 }
