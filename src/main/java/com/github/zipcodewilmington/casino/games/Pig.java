b9adeacc23685ed5522b52e2eef21374ed54c4f9
Bobbi-Z <85585077+Bobbi-Z@users.noreply.github.com>
2021-07-14 21:43:34 -0400

    oops

--- src/main/java/com/github/zipcodewilmington/casino/games/Pig.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/Pig.java
@@ -6,73 +6,77 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Pig{
   public static void main(String[] args){
-   Pig Pig = new Pig();
+    Pig Pig = new Pig();
   }
-    private static int playerTwo = 2;
-    private static int playerOne = 1;
-    private static int scoreToWin = 100;
-    private int currentTurn;
-    private int dieValue;
-    private int rollCounter = 0;
-    private int turnScore;
-    private int pOneTotal;
-    private int pTwoTotal;
-    private int  rollAnswer;
-    private String startTurn = "";
-    public Scanner input = new Scanner(System.in);
-  Random generator = new Random();
- // ThreadLocalRandom current = ThreadLocalRandom.current();
-    //public void welcomeToPig () {
-      //welcome screen
-      //log player 2 in or create account
-      //the rules
+  // private static int playerTwo = 2;
+  // private static int playerOne = 1;
+  // private static int scoreToWin = 100;
+  private int currentTurn;
+  private int dieValue;
+  private int rollCounter = 0;
+  private int turnScore;
+  private int pOneTotal;
+  private int pTwoTotal;
+  private int  rollAnswer;
+  public Scanner input = new Scanner(System.in);
+  //Random generator = new Random();
+  // ThreadLocalRandom current = ThreadLocalRandom.current();
+  //public void welcomeToPig () {
+  //welcome screen
+  //log player 2 in or create account
+  //the rules
   // }
 
   public String currentStateOfTheGame () {
-    return "It is Player " + currentTurn + "'s turn. \n Player 1 has " +
-            pOneTotal + " points. \n Player 2 has " + pTwoTotal + " points.";
+    return "Player 1 has " + pOneTotal + " points. \n Player 2 has " + pTwoTotal + " points.";
   }
 
-  public void playerTurn (){
-    while (rollCounter <= 5)
+  public int playerTurn () {
+
+    do {
       System.out.println("Please enter 1 to roll or 0 to hold");
       //input.nextLine();
       rollAnswer = input.nextInt();
       if (rollAnswer == 1) {
+        Random generator = new Random();
         dieValue = generator.nextInt(6) + 1;
+        turnScore += dieValue;
         // dieValue = ThreadLocalRandom.current().nextInt(1, 7);
         if (dieValue == 1) {
+          currentTurn++;
+          turnScore = 0;
           System.out.println("You rolled a 1; your turn is over and any banked points have been lost.");
-          System.out.println("Next player enter 'r' to roll the die!");
-          input.nextLine();
-          startTurn = input.nextLine();
+          switchingPlayers();
+        } else {
+          System.out.println("You rolled a " + dieValue);
+          rollCounter++;
+          System.out.println("Player earned " + turnScore + " points.");
+          playerTurn();
         }
-      }else {
-        System.out.println("You rolled a " + dieValue);
-        turnScore += dieValue;
-        rollCounter += 1;
-        playerTurn();
+      } else if (rollAnswer == 0) {
+        currentTurn++;
+        switchingPlayers();
       }
-     if (currentTurn % 2 != 1) {
-        pOneTotal += turnScore;
-      } else {
-        pTwoTotal += turnScore;
-      }
-      System.out.println("Player " + currentTurn + "earned " + turnScore + " points.");
-    }
+    } while (currentTurn <= 5);
+
+    return turnScore;
+  }
 
 
   public void switchingPlayers () {
-    System.out.println(currentStateOfTheGame());
     if (currentTurn % 2 == 0) {
+      pOneTotal += turnScore;
       System.out.println("It's Player 2's turn.");
     } else {
+      pTwoTotal += turnScore;
       System.out.println("It's Player 1's turn");
     }
+    turnScore = 0;
+    System.out.println(currentStateOfTheGame());
     playerTurn();
   }
 
-  private boolean getWinner(){
+  public boolean getWinner(){
     if (pOneTotal == 100) {
       System.out.println("Player 1 won!");
     } else if (pTwoTotal == 100) {
@@ -82,12 +86,13 @@ public class Pig{
 
 
   public Pig() {
+
     currentTurn = 1;
     pOneTotal = 0;
     pTwoTotal = 0;
 
     while (!getWinner()) {
-      switchingPlayers();
+      playerTurn();
     }
   }
   }
