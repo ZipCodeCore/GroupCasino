4794127b2d7e3b5b75e41e97ba0c58f84f2c0c95
charnaexw <charnaexw@gmail.com>
2021-07-15 20:10:57 -0400

    Changing Craps Game

--- src/main/java/com/github/zipcodewilmington/casino/games/CrapsGame/CrapsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/CrapsGame/CrapsGame.java
@@ -2,6 +2,7 @@ package com.github.zipcodewilmington.casino.games.CrapsGame;
 
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.utils.AnsiColor;
 import com.github.zipcodewilmington.utils.IOConsole;
 
 import java.io.Console;
@@ -11,18 +12,18 @@ public class CrapsGame implements GameInterface {
     int num1;
     int num2;
     boolean x;
-
+IOConsole console = new IOConsole(AnsiColor.PURPLE);
     public int firstRoll() {
         num1 = dice.rollingTheDice();
 
-        System.out.println("Your first roll is " + num1 + "!");
+        console.println("Your first roll is " + num1 + "!");
         return num1;
     }
 
     public int secondRoll() {
         num2 = dice.rollingTheDice();
 
-        System.out.println("Your second roll is " + num2 + "!");
+        console.println("Your second roll is " + num2 + "!");
         return num2;
     }
 
@@ -30,11 +31,11 @@ public class CrapsGame implements GameInterface {
         boolean x;
         if (num1==7||num1==11){
             if(num2==7||num2==11)
-                return true; System.out.println("Cash Out! You've won!");}
+                return true; console.println("Cash Out! You've won!");}
         if(num1!=11&&num1!=7){
             if(num2!=11&&num2!=7)
                 return true;
-        } System.out.println("Turn in your chips... Better luck next time!");return false;
+        }console.println("Turn in your chips... Better luck next time!"); return false;
     }
 
     @Override
@@ -52,11 +53,15 @@ public class CrapsGame implements GameInterface {
     public void run() {
         IOConsole console = new IOConsole();
         int input = 0;
+        CrapsGame craps = new CrapsGame();
 
         while(input==1){
             console.println("Welcome to the Craps Table!");
-
             input = console.getIntegerInput("Press 1 to roll or press 2 to quit");
+
+            int num1=craps.firstRoll();
+            int num2=craps.secondRoll();
+            craps.sumOfRoll(num1, num2);
 
         }
 
