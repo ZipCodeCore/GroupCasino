868b5f83c4b782a7f07d5cd4f5a758ddc0d92a35
TatianaDeAngelo <85692272+TatianaDeAngelo@users.noreply.github.com>
2021-07-16 06:23:05 -0400

    changes

--- src/main/java/com/github/zipcodewilmington/casino/games/craps/CrapsPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/craps/CrapsPlayer.java
@@ -1,9 +1,10 @@
 package com.github.zipcodewilmington.casino.games.craps;
 
 import com.github.zipcodewilmington.casino.CasinoAccount;
-import com.github.zipcodewilmington.casino.PlayerInterface;
+import com.github.zipcodewilmington.casino.GamblingPlayer;
 
-public class CrapsPlayer implements PlayerInterface {
+
+public class CrapsPlayer implements GamblingPlayer {
 
 
     @Override
@@ -14,5 +15,31 @@ public class CrapsPlayer implements PlayerInterface {
     @Override
     public <SomeReturnType> SomeReturnType play() {
         return null;
+    }
+
+
+    @Override
+    public void increaseBet(float raise) {
+
+    }
+
+    @Override
+    public void decreaseBet(float deposit) {
+
+    }
+
+    @Override
+    public float amountWagered() {
+        return 0;
+    }
+
+    @Override
+    public float amountWon() {
+        return 0;
+    }
+
+    @Override
+    public float payOut() {
+        return 0;
     }
 }
