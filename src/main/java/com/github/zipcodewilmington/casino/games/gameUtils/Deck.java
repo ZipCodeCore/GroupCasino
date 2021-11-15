26677ce62661b1e2eb77feeb4c6942dd5af3d07b
cay <cay@zipcodes-MacBook-Pro-9.local>
2021-11-15 17:27:08 -0500

    adjusted Deck class

--- src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Deck.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/gameUtils/Deck.java
@@ -6,21 +6,19 @@ import java.util.List;
 import java.util.Stack;
 
 public class Deck {
-    List<Card> deckList = new ArrayList<>();
-
-
+//    List<Card> deckList = new ArrayList<>();
 
     //reset deck
 
     //shuffle cards
 
     //draw a card - must be stack method
-    Stack<Card> cardDeck;
+    Stack<Cards> cardDeck = new Stack<>();
 
     public Deck() {
         for(Suit suit : Suit.values()) {
             for (Rank rank : Rank.values()) {
-                cardDeck.push((new Cards(suit,rank)));
+                cardDeck.push(new Cards(suit,rank));
             }
         }
     }
