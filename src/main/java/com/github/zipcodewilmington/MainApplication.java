541eb99006f8ea118c705ac3e6ebaaf59bbd7bcc
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-16 18:08:42 -0500

    merge test

--- src/main/java/com/github/zipcodewilmington/MainApplication.java
+++ src/main/java/com/github/zipcodewilmington/MainApplication.java
@@ -3,5 +3,6 @@ package com.github.zipcodewilmington;
 public class MainApplication {
     public static void main(String[] args) {
         new Casino().run();
+        System.out.println("merge test");
     }
 }
